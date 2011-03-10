7fb7863927af21f139dcb88494b5bab16d013510
Lassi Tuura <lat@cern.ch>
2011-03-10 10:39:41 +0000

    Update DBS to new style deployments, RPM 3.0.6.
    Signed-off-by: Lassi Tuura <lat@cern.ch>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/Infrastructure/branches/deployment-reorg@11877 4525493e-7705-40b1-a816-d608a930855b

--- dbs/DBS.py
+++ dbs/DBS.py
@@ -1,96 +1,74 @@
 """
 DBS Server  configuration file
 """
-import os, logging, os.path
+import os,logging,sys
 from WMCore.Configuration import Configuration
-import WMCore.WMInit
+from WMCore.WMInit import getWMBASE
 
-databaseOwner='fixme_database_account'
+ROOTDIR = os.path.normcase(os.path.abspath(__file__)).rsplit('/', 3)[0]
+DBSVERSION = 'DBS_3_0_6'
 
-CONFIGDIR = os.path.normcase(os.path.abspath(__file__)).rsplit('/', 1)[0]
-LOGDIR = CONFIGDIR.rsplit('/', 1)[0] + "/logs"
-# FIXME: goes too far out of DBS3 installation area?
-# SWDIR = os.path.normcase(os.environ["DBS3_ROOT"]).rsplit('/', 3)[0]
-# FIXME: needs real package dependency? why is this needed here?
-# TEMPLATEDIR = os.path.join(SWDIR, '/cms/wmcore/WMAGENT_0_4_0-cmp/src/templates/WMCore/WebTools/')
+sys.path.append(os.path.join(ROOTDIR,'auth/dbs'))
 
-print CONFIGDIR
-# FIXME print SWDIR
-print LOGDIR
-# FIXME print TEMPLATEDIR
-
-import pdb
-pdb.set_trace()
-
-DBSVersion = 'DBS_3_0_0'
+from DBSSecrets import connectUrlReader
+from DBSSecrets import databaseOwnerReader
+from DBSSecrets import connectUrlWriter
+from DBSSecrets import databaseOwnerWriter
 
 config = Configuration()
+config.component_('SecurityModule')
+config.SecurityModule.key_file = os.path.join(ROOTDIR,'auth/wmcore-auth/header-auth-key')
 
-#Agent section is required by WMCore
-config.section_("Agent")
-config.Agent.contact ="fixme_support_email_here@some.where"
-#Only these two lines in "Agent" are required in order to turn off the default services.
-config.Agent.useMsgService = False
-config.Agent.useTrigger = False
+config.component_('Webtools')
+config.Webtools.port = 8250
+config.Webtools.log_screen = True
+config.Webtools.application = 'cms_dbs'
 
-config.section_("General")
-config.General.workDir = os.environ['DBS3_ROOT']
+config.component_('cms_dbs')
+config.cms_dbs.templates = os.path.join(getWMBASE(),'../src/templates/WMCore/WebTools')
+config.cms_dbs.title = 'DBS Server'
+config.cms_dbs.description = 'CMS DBS Service'
+config.cms_dbs.section_('views')
+config.cms_dbs.admin = 'cmsdbs'
+config.cms_dbs.default_expires = 300
 
-#CoreDatabase section is required by WMCore.
-config.section_("CoreDatabase")
-# User specific parameter
-config.CoreDatabase.version = DBSVersion
-config.CoreDatabase.dbowner = databaseOwner
+active = config.cms_dbs.views.section_('active')
+active.section_('DBSReader')
+active.DBSReader.object = 'WMCore.WebTools.RESTApi'
+active.DBSReader.section_('model')
+active.DBSReader.model.object = 'dbs.web.DBSReaderModel'
+active.DBSReader.section_('formatter')
+active.DBSReader.formatter.object = 'WMCore.WebTools.RESTFormatter'
+
 #Oracle
-#config.CoreDatabase.connectUrl = 'oracle://fixme_database_account:fixme_password@fixme_instance'
-config.CoreDatabase.connectUrl = 'oracle://fixme_database_account:fixme_password@fixme_instance'
-config.CoreDatabase.engineParameters = {'pool_size': 15, 'max_overflow': 10, 'pool_timeout': 200 }
+active.DBSReader.dbowner = databaseOwnerReader
+active.DBSReader.version = DBSVERSION
+active.DBSReader.section_('database')
+active.DBSReader.database.connectUrl = connectUrlReader
+active.DBSReader.database.engineParameters = { 'pool_size': 15, 'max_overflow': 10, 'pool_timeout' : 200 }
 
-#config web server. These are required fields by WMCore althrough some of them are useless.
-config.webapp_("cmsdbs")
-config.cmsdbs.componentDir = LOGDIR+'/DBServer'
-config.cmsdbs.server.host = "::"
-config.cmsdbs.server.port = 8585
-config.cmsdbs.templates = TEMPLATEDIR
-config.cmsdbs.admin = "fixme_admin_email_needed_here@some.where"
-config.cmsdbs.title = 'DBS Global Server'
-config.cmsdbs.dbowner = databaseOwner
-config.cmsdbs.description = 'CMS DBS Global Service'
-config.cmsdbs.default_expires=300
-#config.cmsdbs.index='DBSG'
-config.cmsdbs.section_('views')
-active=config.cmsdbs.views.section_('active')
+active.section_('DBSWriter')
+active.DBSWriter.object = 'WMCore.WebTools.RESTApi'
+active.DBSWriter.section_('model')
+active.DBSWriter.model.object = 'dbs.web.DBSWriterModel'
+active.DBSWriter.section_('formatter')
+active.DBSWriter.formatter.object = 'WMCore.WebTools.RESTFormatter'
 
-#DBS server page/view
-DBSG = active.section_('DBSG')
-DBSG.object = 'WMCore.WebTools.RESTApi'
-DBSG.section_('model')
-DBSG.model.object = 'dbs.web.DBSWriterModel'
-DBSG.section_('formatter')
-active.DBSG.formatter.object = 'WMCore.WebTools.RESTFormatter'
+#Oracle
+active.DBSWriter.dbowner = databaseOwnerWriter
+active.DBSWriter.version = DBSVERSION
+active.DBSWriter.section_('database')
+active.DBSWriter.database.connectUrl = connectUrlWriter
+active.DBSWriter.database.engineParameters = { 'pool_size': 15, 'max_overflow': 10, 'pool_timeout' : 200 }
 
-#Migration server page/view
-MIGRATEG = active.section_('MIGRATEG')
-MIGRATEG.object = 'WMCore.WebTools.RESTApi'
-MIGRATEG.section_('model')
-MIGRATEG.model.object = 'dbs.web.DBSMigrateModel'
-MIGRATEG.section_('formatter')
-MIGRATEG.formatter.object = 'WMCore.WebTools.RESTFormatter'
-MIGRATEG.version = DBSVersion
-MIGRATEG.nthreads = 4
-
-#config migration mover
-config.component_('DBSMigrationMover')
-config.DBSMigrationMover.default_expires=300
-config.DBSMigrationMover.pollInterval = 1
-config.DBSMigrationMover.namespace= "dbs.components.migration.DBSMigrationMover"
-config.DBSMigrationMover.componentDir = LOGDIR + '/DBSGMigrationMover'
-config.DBSMigrationMover.workerThreads = 1
-
-#Config insert buffer
-config.component_('DBSInsertBuffer')
-config.DBSInsertBuffer.default_expires=300
-config.DBSInsertBuffer.pollInterval = 1
-config.DBSInsertBuffer.namespace= "dbs.components.insertbuffer.DBSInsertBuffer"
-config.DBSInsertBuffer.componentDir = LOGDIR + '/DBSGInsertBuffer'
-config.DBSInsertBuffer.workerThreads = 1
+active.section_('MIGRATE')
+active.MIGRATE.object = 'WMCore.WebTools.RESTApi'
+active.MIGRATE.section_('model')
+active.MIGRATE.model.object = 'dbs.web.DBSMigrateModel'
+active.MIGRATE.section_('formatter')
+active.MIGRATE.formatter.object = 'WMCore.WebTools.RESTFormatter'
+active.MIGRATE.section_('database')
+active.MIGRATE.database.connectUrl = connectUrlWriter
+active.MIGRATE.dbowner = databaseOwnerWriter
+active.MIGRATE.version = DBSVERSION
+active.MIGRATE.nthreads = 4
