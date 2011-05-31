8f3684a80edd6b8c47f46eb268d9d8d5c71a05c6
Luis Lopera <lilopera@cern.ch>
2011-05-31 16:02:34 +0000

    DQM GUI configuration updates for HTTP cluster deployment.
    From: Luis Lopera <lilopera@cern.ch>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/Infrastructure/trunk@12856 4525493e-7705-40b1-a816-d608a930855b

---
+++ dqmgui/quota-offline-offline.py
@@ -0,0 +1,6 @@
+{'online_data':1000 * 1024 ** 3,
+ 'offline_data':2500 * 1024 ** 3,
+ 'relval_data':100 * 1024 ** 3,
+ 'relval_mc':70 * 1024 ** 3,
+ 'simulated':10 * 1024 ** 3
+ }
---
+++ dqmgui/quota-offline-relval.py
@@ -0,0 +1,6 @@
+{'online_data':50 * 1024 ** 3,
+ 'offline_data':50 * 1024 ** 3,
+ 'relval_data':2000 * 1024 ** 3,
+ 'relval_mc':1000 * 1024 ** 3,
+ 'simulated':1000 * 1024 ** 3
+ }
