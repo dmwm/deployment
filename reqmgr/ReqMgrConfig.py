7831989866ebc6634a66e0842639550f86eac8b9
Rick Wilkinson <rickw@caltech.edu>
2011-09-14 08:32:09 +0000

    ReqMgr using the 0.8.2 WMCore tag.
    From: Rick Wilkinson <rickw@caltech.edu>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/Infrastructure/trunk@13846 4525493e-7705-40b1-a816-d608a930855b

--- reqmgr/ReqMgrConfig.py
+++ reqmgr/ReqMgrConfig.py
@@ -20,12 +20,13 @@ config = ReqMgrConfig.reqMgrConfig(installation=INSTALL,
   couchurl = COUCH)
 
 config.webapp_("reqmgr")
-config.reqmgr.templates = os.path.join(INSTALL, '../../templates/WMCore/WebTools/RequestManager')
-TEMPLATES= os.path.join(INSTALL, '../../templates/WMCore/WebTools')
-config.reqmgr.views.active.RequestOverview.templates = TEMPLATES
-config.reqmgr.views.active.RequestOverview.javascript = os.path.join(INSTALL, '../../javascript')
-config.reqmgr.views.active.RequestOverview.html  = os.path.join(INSTALL, '../../html')
-config.reqmgr.views.active.reqMgr.templates = TEMPLATES
+
+config.reqmgr.templates = os.path.normpath(os.path.join(INSTALL, '../../templates/WMCore/WebTools/RequestManager'))
+WEBTOOLSTEMPLATES= os.path.normpath(os.path.join(INSTALL, '../../templates/WMCore/WebTools'))
+HTML=os.path.normpath(os.path.join(INSTALL, '../../html/RequestManager'))
+config.reqmgr.html = HTML
+config.reqmgr.views.active.reqMgr.templates = WEBTOOLSTEMPLATES
+config.reqmgr.views.active.rest.templates = WEBTOOLSTEMPLATES
 config.reqmgr.views.active.reqMgr.html = os.path.join(INSTALL, '../../html/RequestManager')
 config.reqmgr.security_roles.extend(['facops', 'FacOps'])
 
