53e41b279d309a40f498ede4be379a0a18fff59d
Diego da Silva Gomes <diego@cern.ch>
2012-01-24 18:18:29 +0000

    Upgrade das to version 1.1.5: new analytics and DASQuery implementation; reduce number of threads.
    From: Diego da Silva Gomes <diego@cern.ch>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/Infrastructure/trunk@14994 4525493e-7705-40b1-a816-d608a930855b

--- das/analytics_cfg.py
+++ das/analytics_cfg.py
@@ -1,6 +1,8 @@
 # DAS analytics web server configuration
-log_to_stdout = 0
-log_to_file = 0
+log_to_stdout = False
+log_to_file = False
+log_to_stderr = False
+logfile_mode = "None"
 web_history = 10000
 minimum_interval = 600
 log_format = "%(asctime)s:%(name)s:%(levelname)s - %(message)s"
@@ -10,15 +12,29 @@ no_start_offset = False
 web = True
 web_port = 8213
 global_das = False
-logfile_mode = "None"
 workers = 4
-log_to_stderr = 0
 web_base = "/analytics"
 pid = '{ROOT}/state/das/das_analytics.pid'
 
-# DAS analytics tasks, use 4 hours interval for statistics analysis
-Task("DatasetHotspot", "ValueHotspot", 14400, key="dataset.name")
-Task("BlockHotspot", "ValueHotspot", 14400, key="block.name")
-Task("FileHotspot", "ValueHotspot", 14400, key="file.name")
-Task("SiteHotspot", "ValueHotspot", 14400, key="site.name")
-Task("RunHotspot", "ValueHotspot", 14400, key="run.run_number")
+# options for results.py
+db_uri  = "mongodb://localhost:8230/"
+db_name = "analytics_results"
+db_col  = "db"
+db_size = 67108864 # 64MB
+
+# Definitions:
+## delta = finish-start (finish = start+period)
+## period: the calls to be considered by analytics, default 1h
+## allowed_gap: is maximum gap in the summary record we are happy to ignore, default 1h
+## interval: interval of the task, suggested value 1h
+#
+# Production tasks
+Task("DatasetHotspot", "ValueHotspot", interval=3600, period=3600, allowed_gap=60, key="dataset.name")
+Task("BlockHotspot", "ValueHotspot", interval=3600, period=3600, allowed_gap=60, key="block.name")
+Task("FileHotspot", "ValueHotspot", interval=3600, period=3600, allowed_gap=60, key="file.name")
+Task("SiteHotspot", "ValueHotspot", interval=3600, period=3600, allowed_gap=60, key="site.name")
+Task("RunHotspot", "ValueHotspot", interval=3600, period=3600, allowed_gap=60, key="run.run_number")
+
+# task to clean-up analytics DB, cutoff parameter defines the
+# threshold for old records, default is 1 month
+Task("CleanUpHotspot", "AnalyticsCleanup", 3600, cutoff=2592000)
