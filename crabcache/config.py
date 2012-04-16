56b489256841f188ac3775a98e4eb9a4c6765472
Lassi Tuura <lat@cern.ch>
2012-04-16 15:19:50 +0000

    Subject: CRAB user file cache server.
    From: Lassi Tuura <lat@cern.ch>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/Infrastructure/trunk@15716 4525493e-7705-40b1-a816-d608a930855b

---
+++ crabcache/config.py
@@ -0,0 +1,23 @@
+from WMCore.Configuration import Configuration
+
+conf = Configuration()
+main = conf.section_('main')
+srv = main.section_('server')
+srv.thread_pool = 5
+main.application = 'crabcache'
+main.port = 8271
+main.index = 'data'
+
+main.authz_defaults = { 'role': None, 'group': None, 'site': None }
+main.section_('tools').section_('cms_auth').key_file = "%s/auth/wmcore-auth/header-auth-key" % __file__.rsplit('/', 3)[0]
+
+app = conf.section_('crabcache')
+app.admin = 'opsmail@cern.ch'
+app.description = 'CRABCache RESTFull API'
+app.title = 'CRABCacheAPIs'
+
+views = conf.section_('views')
+
+data = views.section_('data')
+data.object = 'UserFileCache.RESTBaseAPI.RESTBaseAPI'
+data.cachedir = "%s/state/crabcache/files" % __file__.rsplit('/', 4)[0]
