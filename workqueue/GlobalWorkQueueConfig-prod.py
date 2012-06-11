65b006d7e200b718b61c9bbe85ce44d0e0b8735e
Diego da Silva Gomes <diego@cern.ch>
2012-06-11 16:55:09 +0200

    Add reqmon initial deployment script and related changes to frontend rules and the workqueue configuration.

--- workqueue/GlobalWorkQueueConfig-prod.py
+++ workqueue/GlobalWorkQueueConfig-prod.py
@@ -13,6 +13,7 @@ from WMCore.Configuration import Configuration
 
 workqueueDBName = 'workqueue'
 workqueueInboxDbName = 'workqueue_inbox'
+wmstatDBName = 'wmstats'
 HOST = "cmsweb.cern.ch"
 REQMGR = "https://cmsweb.cern.ch/reqmgr/reqMgr"
 COUCH = "https://cmsweb.cern.ch/couchdb"
@@ -35,9 +36,10 @@ config.component_("WorkQueueManager")
 config.WorkQueueManager.namespace = "WMComponent.WorkQueueManager.WorkQueueManager"
 config.WorkQueueManager.couchurl = COUCH
 config.WorkQueueManager.dbname = workqueueDBName
+config.WorkQueueManager.wmstatDBName = wmstatDBName
 config.WorkQueueManager.inboxDatabase = workqueueInboxDbName
 config.WorkQueueManager.level = "GlobalQueue"
-config.WorkQueueManager.queueParams = {}
+config.WorkQueueManager.queueParams = {'WMStatsCouchUrl': "%s/%s" % (COUCH, wmstatDBName)}
 config.WorkQueueManager.queueParams['QueueURL'] = WEBURL
 config.WorkQueueManager.reqMgrConfig = {}
 config.WorkQueueManager.reqMgrConfig['endpoint'] = REQMGR
