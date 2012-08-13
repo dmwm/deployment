4aecf94ce1033cce250d1dbd101cd0e0e0b63617
gutsche <gutsche@4525493e-7705-40b1-a816-d608a930855b>
2012-08-13 00:13:24 +0000

    merge Oli's local copy and the central copy, added some more protections and more output at the end
    git-svn-id: svn+ssh://svn.cern.ch/reps/cmsfomon/Infrastructure/trunk/Deployment@98 4525493e-7705-40b1-a816-d608a930855b

--- globalmon/parseRequests.py
+++ globalmon/parseRequests.py
@@ -4,18 +4,27 @@ import sys,os,json,subprocess,shlex
 from datetime import datetime
 current = datetime.utcnow()
 
+verbose = False
+
 dbscmd='dbs search --production --noheader '
 
 requests = json.load(sys.stdin)
 
 good_status = ['new', 'assignment-approved', 'acquired','running']
 
+default_time_per_event = {}
+default_time_per_event['ReDigi'] = 30.0
+default_time_per_event['ReReco'] = 30.0
+default_time_per_event['MonteCarloFromGEN'] = 100
+default_time_per_event['MonteCarlo'] = 100
+default_time_per_event['Other'] = 100
+
 keys = []
 details_keys = []
 details2_keys = []
 results = {}
 slots_assign = {'t1' : 20000, 't1_highprio' : 12000, 'mc' : 20000, 'mc_highprio' : 12000, 'production' : 20000}
-site_slots = {'T1_US_FNAL': 10000, 'T1_TW_ASGC': 1500, 'T1_FR_CCIN2P3': 3000, 'T1_IT_CNAF': 2200, 'T1_ES_PIC': 1200, 'T1_DE_KIT': 3000, 'T1_UK_RAL': 2000}
+site_slots = {'T1_US_FNAL': 5500, 'T1_TW_ASGC': 1400, 'T1_FR_CCIN2P3': 2000, 'T1_IT_CNAF': 1500, 'T1_ES_PIC': 900, 'T1_DE_KIT': 1456, 'T1_UK_RAL': 1000}
 
 for request in requests:
     status = request['status']
@@ -34,6 +43,13 @@ for request in requests:
         if key not in details_keys: details_keys.append(key)
     for key in request['details2'].keys():
         if key not in details2_keys: details2_keys.append(key)
+
+    if 'RequestType' in request.keys():
+        if request['RequestType'] == 'Resubmission': continue
+    if 'RequestType' in request['details'].keys():
+        if request['details']['RequestType'] == 'Resubmission': continue
+    if 'RequestType' in request['details2'].keys():
+        if request['details2']['RequestType'] == 'Resubmission': continue
 
     if 'Assignments' in request['details2'].keys():
         assignments = request['details2']['Assignments']
@@ -59,7 +75,11 @@ for request in requests:
         if site.startswith('T1'):
             tmp["t1_sites"].append(site)
 
-    priority = int(request['details']['RequestPriority'])
+    if 'RequestPriority' in request['details'].keys(): 
+        priority = int(request['details']['RequestPriority'])
+    else:
+        print 'Priority could not be extracted from the details for',request_name
+        priority = None
     if priority not in results[local_queue][status].keys(): results[local_queue][status][priority] = []
     tmp["request_name"] = request["request_name"]
     tmp["RequestType"] = request["type"]
@@ -102,16 +122,19 @@ for request in requests:
                 while j < 20 and i + j < len(tmp['BlockWhitelist']):
                     commandline += ' block = ' + str(tmp['BlockWhitelist'][i + j]) + ' or'
                     j += 1
-                commandline += ' block = ' + str(tmp['BlockWhitelist'][i]) + ')" --noheader --production'
+                commandline += ' block = ' + str(tmp['BlockWhitelist'][i]) + ')"'
                 i += j
                 localargs = shlex.split(commandline)
                 output = subprocess.Popen(localargs, shell=False, stdout=subprocess.PIPE)
                 lines = output.communicate()[0].split('\n')
-                try:
-                    evnts += int(lines[0].split()[2])
-                except:
-                    print 'something wrong with:',commandline
+                if len(lines) < 2:
+                    if verbose == True: print 'BlockWhiteList: dbs query for set of runs did not result in any output:',commandline
                     continue
+                else:
+                    if len(lines[0]) >= 3:
+                        evnts += int(lines[0].split()[2])
+                    else:
+                        evnts += 0
             else:
                 tmp["RequestNumEvents"] = evnts
         elif len(tmp['RunWhitelist']) > 0:
@@ -123,36 +146,33 @@ for request in requests:
                 while j < 30 and i + j < len(tmp['RunWhitelist']):
                     commandline += ' run = ' + str(tmp['RunWhitelist'][i + j]) + ' or'
                     j += 1
-                commandline += ' run = ' + str(tmp['RunWhitelist'][i]) + ')" --noheader --production'
+                commandline += ' run = ' + str(tmp['RunWhitelist'][i]) + ')"'
                 i += j
                 localargs = shlex.split(commandline)
                 output = subprocess.Popen(localargs, shell=False, stdout=subprocess.PIPE)
                 lines = output.communicate()[0].split('\n')
-                try:
-                    evnts += int(lines[0].split()[2])
-                except:
-                    print 'something wrong with:',commandline
+                if len(lines) < 2:
+                    if verbose == True: print 'RunWhiteList: dbs query for set of runs did not result in any output:',commandline
                     continue
+                else:
+                    if len(lines[0]) >= 3:
+                        evnts += int(lines[0].split()[2])
+                    else:
+                        evnts += 0
             else:
                 tmp["RequestNumEvents"] = evnts
         else:
-            commandline = dbscmd+'search --query="find dataset,dataset.status,sum(block.numevents) where dataset = '+str(tmp["InputDataset"]) + '" --noheader --production'
+            commandline = dbscmd+'search --query="find dataset,dataset.status,sum(block.numevents) where dataset = '+str(tmp["InputDataset"]) + '"'
             localargs = shlex.split(commandline)
             output = subprocess.Popen(localargs, shell=False, stdout=subprocess.PIPE)
             lines = output.communicate()[0].split('\n')
-            try:
+            if len(lines) < 2:
+                if verbose == True: print 'dbs query for set of runs did not result in any output:',commandline
+            else:
                 tmp["RequestNumEvents"] = int(lines[0].split()[2])
-            except:
-                print 'something wrong with:',commandline
-                continue
-        # das_result = json.loads(get_data('https://cmsweb.cern.ch', 'dataset='+tmp["InputDataset"]+' | grep dataset.nevents',0,0,False))
-        # print das_result
-        # for item in das_result['data'][0]['dataset']:
-        #     if 'nevents' in item.keys():
-        #         tmp["RequestNumEvents"] = item['nevents']
-        #         break
-        if tmp["RequestNumEvents"] == None :
-            print "Could not determine number of events for dataset",tmp["InputDataset"]
+        if tmp["RequestNumEvents"] == None or tmp["RequestNumEvents"] == 0:
+            print "Could not determine number of events for dataset",tmp["InputDataset"],"for request",request_name,", setting to 1"
+            tmp["RequestNumEvents"] = 1
         else:
             tmp['FilterEfficiency'] = request.get('FilterEfficiency', None)
             if tmp['FilterEfficiency'] == None : tmp['FilterEfficiency'] = request['details'].get('FilterEfficiency', None)
@@ -161,16 +181,16 @@ for request in requests:
                 tmp['RequestNumEvents'] *= float(tmp['FilterEfficiency'])
                 tmp['RequestNumEvents'] = int(tmp['RequestNumEvents'])
         if tmp["RequestType"] == 'ReDigi':
-            tmp["TimePerEvent"] = 30.
+            tmp["TimePerEvent"] = default_time_per_event['ReDigi']
         elif tmp["RequestType"] == 'ReReco':
-            tmp["TimePerEvent"] = 30.
+            tmp["TimePerEvent"] = default_time_per_event['ReReco']
         elif tmp["RequestType"] == 'MonteCarloFromGEN':
             if 'TimePerEvent' in request['details']:
                 tmp["TimePerEvent"] = float(request['details']["TimePerEvent"])
             else :
-                tmp["TimePerEvent"] = 100.
+                tmp["TimePerEvent"] = default_time_per_event['MonteCarloFromGEN']
         else:
-            tmp["TimePerEvent"] = 30.
+            tmp["TimePerEvent"] = default_time_per_event['Other']
         tmp["CPUHours"] = tmp["RequestNumEvents"] * tmp["TimePerEvent"] / 3600.
     tmp['OutputDatasets'] = request['OutputDatasets']
     tmp['OutputEvents'] = {}
@@ -180,7 +200,7 @@ for request in requests:
             dataTier = str(outputDataset)[1:].split('/')[2]
             acqEra = str(outputDataset)[1:].split('/')[1]
             if acqEra != 'None-None':
-                commandline = dbscmd+'search --query="find dataset,dataset.status,sum(block.numevents) where dataset = '+str(outputDataset) + '" --noheader --production'
+                commandline = dbscmd+'search --query="find dataset,dataset.status,sum(block.numevents) where dataset = '+str(outputDataset) + '"'
                 localargs = shlex.split(commandline)
                 output = subprocess.Popen(localargs, shell=False, stdout=subprocess.PIPE)
                 lines = output.communicate()[0].split('\n')
@@ -190,11 +210,24 @@ for request in requests:
                     tmp["OutputEvents"][dataTier] = int(lines[0].split()[2])
             else:
                 tmp["OutputEvents"][dataTier] = 0
-            tmp["CompletionPercentage"][dataTier] = float(tmp["OutputEvents"][dataTier])/float(tmp["RequestNumEvents"])
+            if tmp["RequestNumEvents"] == None or tmp["RequestNumEvents"] == 0.:
+                print 'problems with RequestNumEvents in workflow',tmp["request_name"]
+                tmp["CompletionPercentage"][dataTier] = 0.
+            else :
+                tmp["CompletionPercentage"][dataTier] = float(tmp["OutputEvents"][dataTier])/float(tmp["RequestNumEvents"])
     results[local_queue][status][priority].append(tmp)
 
 print '--------------------------------------------------------------------------------'
 print 'Report generated on',current,'UTC'
+print '--------------------------------------------------------------------------------'
+
+print '--------------------------------------------------------------------------------'
+print 'Defaults per workflow type for time per event if not specified in workflow:'
+print "ReReco            : %6.2fs" % default_time_per_event['ReReco']
+print "ReDigi            : %6.2fs" % default_time_per_event['ReDigi']
+print "MonteCarloFromGEN : %6.2fs" % default_time_per_event['MonteCarloFromGEN']
+print "MonteCarlo        : %6.2fs" % default_time_per_event['MonteCarlo']
+print "Other             : %6.2fs" % default_time_per_event['Other']
 print '--------------------------------------------------------------------------------'
 
 sorted_local_queues = results.keys()
@@ -205,13 +238,13 @@ for local_queue in sorted_local_queues:
     print ''
     print ''
     print 'team:',local_queue
-    print '============================================'
+    print '================================================================================'
     team_total_CPU = 0.0
     for status in good_status:
         if status not in results[local_queue].keys(): continue
         print ''
         print 'status:',status
-        print '--------------------------------------------'
+        print '--------------------------------------------------------------------------------'
         sorted_priority = results[local_queue][status].keys()
         sorted_priority.sort(reverse=True)
         for priority in sorted_priority:
@@ -246,9 +279,14 @@ for local_queue in sorted_local_queues:
                     for tier in item['CompletionPercentage'].keys():
                         if tier != 'DQM':
                             usablePercentages[tier] = item['CompletionPercentage'][tier]
-                    team_total_CPU += item["CPUHours"]*(1.0 - min(usablePercentages.values()))
-                    if len(item["t1_sites"]) > 0:
-                        site_summary[item["t1_sites"][0]]['totalCPUHours'] += item["CPUHours"]*(1.0 - min(usablePercentages.values()))
+                    if len(usablePercentages.values()) > 0 :
+                        team_total_CPU += item["CPUHours"]*(1.0 - min(usablePercentages.values()))
+                        if len(item["t1_sites"]) > 0:
+                            site_summary[item["t1_sites"][0]]['totalCPUHours'] += item["CPUHours"]*(1.0 - min(usablePercentages.values()))
+                    else :
+                        team_total_CPU += item["CPUHours"]
+                        if len(item["t1_sites"]) > 0:
+                            site_summary[item["t1_sites"][0]]['totalCPUHours'] += item["CPUHours"]
     team_summary[local_queue] = {'cpuHours' : team_total_CPU, 'days' : 0, 'hours' : 0}
     if local_queue in slots_assign.keys():
         total_hours = team_total_CPU/slots_assign[local_queue]
@@ -260,13 +298,70 @@ for local_queue in sorted_local_queues:
 print ''
 print ''
 print 'Team summary:'
-print '============================================'
+print '================================================================================'
 for team in sorted(team_summary.keys(), key = lambda x: team_summary[x]['cpuHours'], reverse=True):
     print 'team: %12s CPU Hours remaining: %12d "Real" time remaining: %3d days %2d hours Approximated slots: %2dk' %  (team, team_summary[team]['cpuHours'], team_summary[team]['days'], team_summary[team]['hours'], slots_assign.get(team, 0)/1000)
 
+# print ''
+# print ''
+# print 'Custodial T1 Site summary:'
+# print '============================================'
+# for site in sorted(site_summary.keys(), key = lambda x: site_summary[x]['totalCPUHours'], reverse=True):
+#     print 'site: %14s CPU Hours remaining: %12d Workflows assigned: %4d maximum slots: %5d' % (site, site_summary[site]['totalCPUHours'], site_summary[site]['workflows'], site_slots[site])
+
+ofInterest = {}
+ofInterest["13Jul"] = {"campaign":"13Jul2012","tier":["AOD"],"time_per_event":15.0}
+ofInterest["428_R2"] = {"campaign":"Upgrade MC","tier":["AOD","RECO"],"time_per_event":120.0}
+ofInterest["Summer12_DR53X"] = {"campaign":"Summer12_DR53X","tier":["AODSIM"],"time_per_event":17.5}
+
+estimates = {}
+
+good_status = ['acquired','running']
+for local_queue in sorted_local_queues:
+    for status in good_status:
+        if status not in results[local_queue].keys(): continue
+        sorted_priority = results[local_queue][status].keys()
+        sorted_priority.sort(reverse=True)
+        for priority in sorted_priority:
+            for item in results[local_queue][status][priority]:
+                for interest in ofInterest.keys():
+                    if item['request_name'].count(interest) > 0:
+                        if interest not in estimates.keys(): estimates[interest] = {}
+                        # add up remaining CPU hours per T1 site
+                        if len(item["t1_sites"]) <= 0: 
+                            print 'no t1 site assigned for request',item['request_name'] / 3600.
+                            break
+                        assignedT1 = item["t1_sites"][0]
+                        total_CPU = 0
+                        if len(item['CompletionPercentage']) == 0:
+                            total_CPU = item["RequestNumEvents"] * ofInterest[interest]['time_per_event'] / 3600.
+                        else:
+                            for tier in ofInterest[interest]['tier']:
+                                if tier in item['CompletionPercentage'].keys():
+                                    total_CPU += item["RequestNumEvents"] * ofInterest[interest]['time_per_event']  / 3600. * (1.0 - item['CompletionPercentage'][tier])
+                                    break
+                                else:
+                                    total_CPU = item["RequestNumEvents"] * ofInterest[interest]['time_per_event'] / 3600.
+                        if assignedT1 not in estimates[interest].keys(): estimates[interest][assignedT1] = 0.
+                        estimates[interest][assignedT1] += total_CPU
+                        
+                        
 print ''
 print ''
-print 'Custodial T1 Site summary:'
-print '============================================'
-for site in sorted(site_summary.keys(), key = lambda x: site_summary[x]['totalCPUHours'], reverse=True):
-    print 'site: %14s CPU Hours remaining: %12d Workflows assigned: %4d maximum slots: %5d' % (site, site_summary[site]['totalCPUHours'], site_summary[site]['workflows'], site_slots[site])
+print 'Estimates for different campaigns:'
+print '================================================================================'
+print ''
+
+print '--------------------------------------------------------------------------------'
+print 'Defaults per campaign for time per event if not specified in workflow:'
+for interest in ofInterest.keys():
+    print "%20s : %6.2fs" % (ofInterest[interest]["campaign"],ofInterest[interest]["time_per_event"])
+print '--------------------------------------------------------------------------------'
+print ''
+
+for interest in estimates.keys():
+    print 'estimates for campaign ' + ofInterest[interest]['campaign'] + " querying for *" + interest + "*"
+    print '--------------------------------------------------------------------------------'
+    for tier1 in estimates[interest].keys():
+        print "Tier1: %14s has %5d slots and still to run %12d CPUhours which corresponds to %6.2f days" % (tier1,site_slots[tier1],estimates[interest][tier1],estimates[interest][tier1]/site_slots[tier1]/24.)
+    print ''
