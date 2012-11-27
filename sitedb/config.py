0ba3ec3d48de969d73cfa90709263abd440a2f6b
Diego da Silva Gomes <diego@cern.ch>
2012-11-27 14:18:29 +0100

    Enable the Ldap syncer in the cmsweb-dev for testing.

--- sitedb/config.py
+++ sitedb/config.py
@@ -8,6 +8,11 @@ HOST = socket.gethostname().lower()
 KEY_FILE = "%s/auth/wmcore-auth/header-auth-key" % __file__.rsplit('/', 3)[0]
 config = Config(nthreads = THREADS, authkey = KEY_FILE)
 
+if HOST.startswith("vocms127"):
+  config.views.data.ldapsync = True
+  config.views.data.ldsyncto = "dev"
+  config.views.data.ldsynctime = 300
+
 # config.main.tools.cms_auth.policy = 'dangerously_insecure'
 # config.main.server.environment = 'staging'
 # config.main.server.socket_host = '127.0.0.1'
