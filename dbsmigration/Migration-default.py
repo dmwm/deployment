d5f3587aa1a65c09e28a195c28df40bff286943e
Diego da Silva Gomes <diego@cern.ch>
2013-01-23 09:16:07 +0100

    DBS HG1302 updates. Closes #54.

--- dbsmigration/Migration-default.py
+++ dbsmigration/Migration-default.py
@@ -8,9 +8,9 @@ ROOTDIR = os.path.normcase(os.path.abspath(__file__)).rsplit('/', 3)[0]
 
 sys.path.append(os.path.join(ROOTDIR,'auth/dbs'))
 
-from DBSSecrets import dbs3_l3_i2
-from DBSSecrets import dbs3_l2_i2
-from DBSSecrets import dbs3_l1_i2
+from DBSSecrets import dbs3_pl1_r
+from DBSSecrets import dbs3_dg_i2
+from DBSSecrets import dbs3_ig_i2
 
 config = Configuration()
 
@@ -21,22 +21,24 @@ config.web.log_screen = True
 config.web.thread_pool = 10
 
 config.component_('dbsmigration')
-config.dbsmigration.threads = 4
 config.dbsmigration.instances = ['prod/global','dev/global','int/global']
 config.dbsmigration.section_('database')
 db_instances = config.dbsmigration.database.section_('instances')
 
 db_production_global = db_instances.section_('prod/global')
-db_production_global.dbowner = dbs3_l2_i2['databaseOwner']
-db_production_global.connectUrl = dbs3_l2_i2['connectUrl']['writer']
+db_production_global.threads = 2
+db_production_global.dbowner = dbs3_pl1_r['databaseOwner']
+db_production_global.connectUrl = dbs3_pl1_r['connectUrl']['writer']
 db_production_global.engineParameters = { 'pool_size' : 15, 'max_overflow' : 10, 'pool_timeout' : 200 }
 
 db_development_global = db_instances.section_('dev/global')
-db_development_global.dbowner = dbs3_l1_i2['databaseOwner']
-db_development_global.connectUrl = dbs3_l1_i2['connectUrl']['writer']
+db_development_global.dbowner = dbs3_dg_i2['databaseOwner']
+db_development_global.threads = 1
+db_development_global.connectUrl = dbs3_dg_i2['connectUrl']['writer']
 db_development_global.engineParameters = { 'pool_size' : 15, 'max_overflow' : 10, 'pool_timeout' : 200 }
 
 db_integration_global = db_instances.section_('int/global')
-db_integration_global.dbowner = dbs3_l3_i2['databaseOwner']
-db_integration_global.connectUrl = dbs3_l3_i2['connectUrl']['writer']
+db_integration_global.threads = 1
+db_integration_global.dbowner = dbs3_ig_i2['databaseOwner']
+db_integration_global.connectUrl = dbs3_ig_i2['connectUrl']['writer']
 db_integration_global.engineParameters = { 'pool_size' : 15, 'max_overflow' : 10, 'pool_timeout' : 200 }
