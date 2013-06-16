f48da2886618cd3664e7bd859daac4a272e0daf8
cinquo <m.cinquilli@gmail.com>
2013-06-17 00:36:32 +0200

    crabserver-3.2.0pre9 release update for configuration, HG1306. Closes #76.

--- crabserver/config.py
+++ crabserver/config.py
@@ -1,4 +1,8 @@
 from WMCore.Configuration import Configuration
+import re
+import socket
+
+myhost = socket.getfqdn().lower()
 
 conf = Configuration()
 main = conf.section_('main')
@@ -21,12 +25,20 @@ views = conf.section_('views')
 data = views.section_('data')
 data.object = 'CRABInterface.RESTBaseAPI.RESTBaseAPI'
 data.transformation = 'http://common-analysis-framework.cern.ch/CMSRunAnaly.sh'
-data.workflowManager = 'PandaDataWorkflow'
 data.phedexurl = 'https://cmsweb.cern.ch/phedex/datasvc/xml/prod/'
 data.dbsurl = 'http://cmsdbsprod.cern.ch/cms_dbs_prod_global/servlet/DBSServlet'
 data.defaultBlacklist = ['T0_CH_CERN']
 data.serverhostcert = "%s/auth/crabserver/dmwm-service-cert.pem" % __file__.rsplit('/', 3)[0]
 data.serverhostkey = "%s/auth/crabserver/dmwm-service-key.pem" % __file__.rsplit('/', 3)[0]
-data.delegatedn = []
 data.credpath = '%s/state/crabserver/proxy/' % __file__.rsplit('/', 4)[0]
 data.db = 'CRABServerAuth.dbconfig'
+data.workflowManager = 'PandaDataWorkflow'
+
+extconfig = 'cmsweb-dev'
+if re.match(r"^vocms(?:10[67]|13[689]|140|16[13])\.cern\.ch$", myhost):
+  extconfig = "cmsweb-prod"
+elif re.match(r"^vocms(?:13[23])\.cern\.ch$", myhost):
+  extconfig = "cmsweb-preprod"
+elif re.match(r"^vocms127\.cern\.ch$", myhost):
+  extconfig = "cmsweb-dev"
+data.extconfigurl = 'http://git.cern.ch/pubweb/?p=CAFServicesConfig.git;a=blob_plain;f=%s/rest-config.json' %extconfig
