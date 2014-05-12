ef3850d9c7eaf547ff14f96c7cb57d809fc70166
Marco Mascheroni <marco.mascheroni@cern.ch>
2014-05-12 14:15:23 +0200

    Add rotation of logfile in the configuration

--- crabserver/config.py
+++ crabserver/config.py
@@ -1,6 +1,7 @@
 from WMCore.Configuration import Configuration
 import re
 import socket
+import time
 
 myhost = socket.getfqdn().lower()
 
@@ -37,4 +38,4 @@ data.workflowManager = 'HTCondorDataWorkflow'
 data.extconfigurl = 'http://git.cern.ch/pubweb/?p=CAFServicesConfig.git;a=blob_plain;f=cmsweb-rest-config.json'
 
 data.loggingLevel = 10
-data.loggingFile = '%s/logs/crabserver/CRAB.log' % __file__.rsplit('/', 4)[0]
+data.loggingFile = '%s/logs/crabserver/CRAB-%s.log' % (__file__.rsplit('/', 4)[0], time.strftime("%Y%m%d"))
