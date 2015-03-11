1f9a32511933bbbef2479810a54016520d1ae8f6
Seangchan Ryu <sryu@fnal.gov>
2015-03-11 11:22:06 -0500

    add reqmgr2 config in workqueue and wmagent

--- workqueue/GlobalWorkQueueConfig-dev.py
+++ workqueue/GlobalWorkQueueConfig-dev.py
@@ -17,6 +17,7 @@ wmstatDBName = 'wmstats'
 reqmgrCouchDB = "reqmgr_workload_cache"
 HOST = "cmsweb-dev.cern.ch"
 REQMGR = "https://cmsweb-dev.cern.ch/reqmgr/reqMgr"
+REQMGR2 = "https://%s/reqmgr2" % HOST
 COUCH = "https://cmsweb-dev.cern.ch/couchdb"
 WEBURL = "%s/%s" % (COUCH, workqueueDBName)
 
@@ -40,6 +41,9 @@ config.WorkQueueManager.wmstatDBName = wmstatDBName
 config.WorkQueueManager.level = "GlobalQueue"
 config.WorkQueueManager.queueParams = {'WMStatsCouchUrl': "%s/%s" % (COUCH, wmstatDBName)}
 config.WorkQueueManager.queueParams['QueueURL'] = WEBURL
-config.WorkQueueManager.queueParams['ReqMgrServiceURL'] = REQMGR
+config.WorkQueueManager.queueParams['ReqMgrServiceURL'] = REQMGR2
 config.WorkQueueManager.reqMgrConfig = {}
 config.WorkQueueManager.reqMgrConfig['endpoint'] = REQMGR
+# when reqmgr2 is ready change following to endpoint and reqmgr2_only to True
+config.WorkQueueManager.reqMgrConfig['reqmgr2_endpoint'] = REQMGR2
+config.WorkQueueManager.reqMgrConfig['reqmgr2_only'] = False
