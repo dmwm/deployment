445ffdffa315cfe506c3e71ab8551dbb205a8481
Broen van Besien <broen.van.besien@cern.ch>
2015-04-27 11:18:37 +0200

    Changed root file quota for dev server

--- dqmgui/quota-rfqc-dev.py
+++ dqmgui/quota-rfqc-dev.py
@@ -1,8 +1,8 @@
-{'online_data':1 * 1024 ** 3,
- 'offline_data':5 * 1024 ** 3,
- 'relval_data':5 * 1024 ** 3,
- 'relval_mc':5 * 1024 ** 3,
- 'relval_rundepmc':5 * 1024 ** 3,
+{'online_data':20 * 1024 ** 3,
+ 'offline_data':200 * 1024 ** 3,
+ 'relval_data':20 * 1024 ** 3,
+ 'relval_mc':20 * 1024 ** 3,
+ 'relval_rundepmc':20 * 1024 ** 3,
  'simulated_rundep':5 * 1024 ** 3,
  'simulated':5 * 1024 ** 3
 }
