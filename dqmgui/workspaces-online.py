d6c3b273f429dc3efc134e03c8f00514b0b0e112
Viktor Khristenko <vdkhristenko1991@gmail.com>
2015-10-08 04:08:53 +0200

    Polishing

--- dqmgui/workspaces-online.py
+++ dqmgui/workspaces-online.py
@@ -94,7 +94,7 @@ server.workspace('DQMContent', 30, 'Calorimeter', 'HCAL', '^Hcal/', '',
 				 'Hcal/Layouts/13 HCAL Occupancies vs LS',
 				 'Hcal/Layouts/14 HCAL HF Timing',
 				 'Hcal/Layouts/15 HCAL HBHE RecHit Timing',
-                 'Hcal/Layouts/16 HCAL D1&D2 Digi and RecHit Occupancies after Cuts',
+                 'Hcal/Layouts/16 HCAL D1&D2 Digi and RecHit Occupancies with Cuts',
 				 'Hcal/Layouts/17 HCAL HFP&HFM RecHit iphi-dependent Occupancies')
 
 server.workspace('DQMContent',30,'Calorimeter','HCALcalib', '^HcalCalib/', '',
