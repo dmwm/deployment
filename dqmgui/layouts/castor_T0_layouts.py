cc43a1e129682a4b09c13ae23cf09c352ac1178c
Igor <igor.i.katkov@gmail.com>
2015-12-10 18:49:13 +0100

    More clear plot names

--- dqmgui/layouts/castor_T0_layouts.py
+++ dqmgui/layouts/castor_T0_layouts.py
@@ -1,16 +1,16 @@
 def castorlayout(i, p, *rows): i["Castor/Layouts/" + p] = DQMItem(layout=rows)
 
-castorlayout(dqmitems, "00 Castor quick collection plot",
+castorlayout(dqmitems, "01 - Map of frontend and readout errors",
            [{ 'path': "Castor/CastorDigiMonitor/QIE_capID+er+dv",
              'description':"Frontend and readout errors"}]
            )
 
-castorlayout(dqmitems, "01 Castor quick collection plot",
+castorlayout(dqmitems, "02 - Channel-wise timing",
            [{ 'path': "Castor/CastorDigiMonitor/QfC=f(Tile TS) (cumulative)",
              'description':"Channel-wise timing"}]
            )
 
-castorlayout(dqmitems, "02 Castor quick collection plot",
+castorlayout(dqmitems, "03 - Map of rechit occupancies",
            [{ 'path': "Castor/CastorRecHitMonitor/CastorRecHitOccMap",
              'description':"Map of rechit occupancies"}]
            )
--- dqmgui/layouts/shift_castor_T0_layout.py
+++ dqmgui/layouts/shift_castor_T0_layout.py
@@ -1,16 +1,16 @@
 def shiftcastorlayout(i, p, *rows): i["00 Shift/Castor/" + p] = DQMItem(layout=rows)
 
-shiftcastorlayout(dqmitems, "00 Castor shifter plot",
+shiftcastorlayout(dqmitems, "01 - Map of frontend and readout errors",
            [{ 'path': "Castor/CastorDigiMonitor/QIE_capID+er+dv",
              'description':"Frontend and readout errors"}]
            )
 
-shiftcastorlayout(dqmitems, "01 Castor shifter plot",
+shiftcastorlayout(dqmitems, "02 - Channel-wise timing",
            [{ 'path': "Castor/CastorDigiMonitor/QfC=f(Tile TS) (cumulative)",
              'description':"Channel-wise timing"}]
            )
 
-shiftcastorlayout(dqmitems, "02 Castor shifter plot",
+shiftcastorlayout(dqmitems, "03 - Map of rechit occupancies",
            [{ 'path': "Castor/CastorRecHitMonitor/CastorRecHitOccMap",
              'description':"Map of rechit occupancies"}]
            )
