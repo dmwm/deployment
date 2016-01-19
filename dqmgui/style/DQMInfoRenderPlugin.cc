5740ca0753700f9224fbea3b4ac759822819f29a
Broen van Besien <broen.van.besien@cern.ch>
2016-01-19 14:40:34 +0100

    Updated renderer for hv plot. Close #275.

--- dqmgui/style/DQMInfoRenderPlugin.cc
+++ dqmgui/style/DQMInfoRenderPlugin.cc
@@ -59,7 +59,7 @@ public:
         s=dynamic_cast<TObjString*>(o.object);
         taglist=s->String();
         s->SetString("");
-	   }
+           }
   }
 
  virtual void postDraw( TCanvas *c, const VisDQMObject &o, const VisDQMImgInfo & )
@@ -104,136 +104,136 @@ public:
       if ( o.name.find( "Info/EventInfo/reportSummaryMap" ) != std::string::npos )
       {
          // object is TH2 histogram
-	 TH2F* obj = dynamic_cast<TH2F*>( o.object );
-	 assert( obj );
+         TH2F* obj = dynamic_cast<TH2F*>( o.object );
+         assert( obj );
 
          int topbin = obj->GetNbinsY();
          for ( int i = 1 ; i < obj->GetNbinsX(); i++ )
-	 {
-	   {
-  	     if ( obj->GetBinContent(i,topbin-1) == 1 )
-	     {
+         {
+           {
+             if ( obj->GetBinContent(i,topbin-1) == 1 )
+             {
                char s[25];
-  	       sprintf (s,"Collisions10");
+               sprintf (s,"Collisions10");
                TText tt1;
-	       tt1.SetTextSize(0.15);
-	       tt1.SetTextColor(13);
-	       tt1.DrawTextNDC(0.2,0.5,const_cast<char*>(s));
-	       if ( obj->GetBinContent(i,topbin-2) == 1 )
-	       {
-    		 sprintf (s,"7 TeV");
-        	 TText tt2;
-		 tt2.SetTextSize(0.1);
-		 tt2.SetTextColor(13);
-		 tt2.DrawTextNDC(0.4,0.4,const_cast<char*>(s));
- 	       }
-	       break;
-	     }
-	   }
+               tt1.SetTextSize(0.15);
+               tt1.SetTextColor(13);
+               tt1.DrawTextNDC(0.2,0.5,const_cast<char*>(s));
+               if ( obj->GetBinContent(i,topbin-2) == 1 )
+               {
+                 sprintf (s,"7 TeV");
+                 TText tt2;
+                 tt2.SetTextSize(0.1);
+                 tt2.SetTextColor(13);
+                 tt2.DrawTextNDC(0.4,0.4,const_cast<char*>(s));
+               }
+               break;
+             }
+           }
          }
       }
       else if ( o.name.find( "Info/LhcInfo/beamMode") != std::string::npos )
       {
-	 TH1F* obj = dynamic_cast<TH1F*>( o.object );
-	 assert( obj );
+         TH1F* obj = dynamic_cast<TH1F*>( o.object );
+         assert( obj );
 
-	 std::string smode[22] =
-	 {
-	  "",
-	  "no mode",
-	  "setup",
-	  "inj pilot",
-	  "inj intr",
-	  "inj nomn",
-	  "pre ramp",
-	  "ramp",
-	  "flat top",
-	  "squeeze",
-	  "adjust",
-	  "stable beams",
-	  "unstable",
-	  "beam dump",
-	  "ramp down",
-	  "recovery",
-	  "inj dump",
-	  "circ dump",
-	  "abort",
-	  "cycling",
-	  "warn beam dump",
-	  "no beam"
-	 };
+         std::string smode[22] =
+         {
+          "",
+          "no mode",
+          "setup",
+          "inj pilot",
+          "inj intr",
+          "inj nomn",
+          "pre ramp",
+          "ramp",
+          "flat top",
+          "squeeze",
+          "adjust",
+          "stable beams",
+          "unstable",
+          "beam dump",
+          "ramp down",
+          "recovery",
+          "inj dump",
+          "circ dump",
+          "abort",
+          "cycling",
+          "warn beam dump",
+          "no beam"
+         };
 
-	 int ibin=0;
-	 for ( int i = obj->GetNbinsX(); i > 0; --i )
-	 {
-	   if ( obj->GetBinContent(i) != 0 )
-	   {
-	      ibin = (int)obj->GetBinContent(i);
-	      break;
-	   }
-	 }
+         int ibin=0;
+         for ( int i = obj->GetNbinsX(); i > 0; --i )
+         {
+           if ( obj->GetBinContent(i) != 0 )
+           {
+              ibin = (int)obj->GetBinContent(i);
+              break;
+           }
+         }
 
          if (ibin>2)
-	 {
+         {
            const char* s = smode[ibin].c_str();
            TText tt;
-	   tt.SetTextSize(0.08);
+           tt.SetTextSize(0.08);
            tt.SetTextColor(2);
-	   if (ibin == 11)
-	     tt.SetTextColor(3);
-	   tt.DrawTextNDC(0.3,0.5,s);
-	 }
+           if (ibin == 11)
+             tt.SetTextColor(3);
+           tt.DrawTextNDC(0.3,0.5,s);
+         }
 
       }
       else if ( o.name.find( "Info/LhcInfo/lhcFill") != std::string::npos )
       {
-	 TH1F* obj = dynamic_cast<TH1F*>( o.object );
-	 assert( obj );
+         TH1F* obj = dynamic_cast<TH1F*>( o.object );
+         assert( obj );
 
-	 int y=0;
-	 for ( int i = obj->GetNbinsX(); i > 0; --i )
-	 {
-	   if ( obj->GetBinContent(i) != 0 )
-	   {
-	      y = (int)obj->GetBinContent(i);
-	      break;
-	   }
-	 }
+         int y=0;
+         for ( int i = obj->GetNbinsX(); i > 0; --i )
+         {
+           if ( obj->GetBinContent(i) != 0 )
+           {
+              y = (int)obj->GetBinContent(i);
+              break;
+           }
+         }
 
          if (y!=0 && y<9999)
-	 {
+         {
            char s[25];
-	   sprintf (s,"LHC Fill: %d",y);
+           sprintf (s,"LHC Fill: %d",y);
            TText tt;
-	   tt.SetTextSize(0.06);
-	   tt.SetTextColor(4);
-	   tt.DrawTextNDC(0.2,0.5,const_cast<char*>(s));
-	 }
+           tt.SetTextSize(0.06);
+           tt.SetTextColor(4);
+           tt.DrawTextNDC(0.2,0.5,const_cast<char*>(s));
+         }
       }
       else if ( o.name.find( "Info/LhcInfo/momentum") != std::string::npos )
       {
-	 TH1F* obj = dynamic_cast<TH1F*>( o.object );
-	 assert( obj );
+         TH1F* obj = dynamic_cast<TH1F*>( o.object );
+         assert( obj );
 
-	 int y=0;
-	 for ( int i = obj->GetNbinsX(); i > 0; --i )
-	 {
-	   if ( obj->GetBinContent(i) != 0 )
-	   {
-	      y = (int)obj->GetBinContent(i);
-	      break;
-	   }
-	 }
+         int y=0;
+         for ( int i = obj->GetNbinsX(); i > 0; --i )
+         {
+           if ( obj->GetBinContent(i) != 0 )
+           {
+              y = (int)obj->GetBinContent(i);
+              break;
+           }
+         }
 
          if (y!=0 && y<9999)
-	 {
+         {
            char s[25];
-	   sprintf (s,"Beam Energy: %d GeV",y);
+           sprintf (s,"Beam Energy: %d GeV",y);
            TText tt;
-	   tt.SetTextSize(0.06);
-	   tt.SetTextColor(4);
-	   tt.DrawTextNDC(0.2,0.5,const_cast<char*>(s));
-	 }
+           tt.SetTextSize(0.06);
+           tt.SetTextColor(4);
+           tt.DrawTextNDC(0.2,0.5,const_cast<char*>(s));
+         }
       }
 
     }
@@ -250,11 +250,19 @@ private:
 
       for ( int i = nbins; i > 0; --i )
       {
-	if ( obj->GetBinContent(i,topBin) != 0 )
-	{
-	   maxRange = TMath::Max(i+1,2);  // leave at least 2 bins
-	   break;
-	}
+        if ( obj->GetBinContent(i,topBin) != 0 )
+        {
+          // We make an extra white band to the right of the plot for better
+          // visibility. (Was originally done in the code, but this belongs
+          // more in the render plugin.)
+          for ( int j = 1; j <= topBin; j++ ) {
+              obj->SetBinContent(i + 1, j, -1.);
+          }
+          // Set the range of the plot to span up to the last filled bin of
+          // the top row + the extra row we added:
+          maxRange = TMath::Max(i + 1, 2);  // leave at least 2 bins
+          break;
+        }
       }
 
       obj->GetXaxis()->SetRange(1,maxRange);
@@ -273,10 +281,10 @@ private:
 
       for( int i=0; i<2; i++ )
       {
-	pcol[i] = 801+i;
-	TColor* color2 = gROOT->GetColor( 801+i );
-	if( ! color2 ) color2 = new TColor( 801+i, 0, 0, 0, "" );
-	color2->SetRGB( rgb[i][0], rgb[i][1], 0. );
+        pcol[i] = 801+i;
+        TColor* color2 = gROOT->GetColor( 801+i );
+        if( ! color2 ) color2 = new TColor( 801+i, 0, 0, 0, "" );
+        color2->SetRGB( rgb[i][0], rgb[i][1], 0. );
       }
 
       gStyle->SetPalette(2, pcol);
@@ -297,11 +305,11 @@ private:
       int maxRange = nbins;
       for ( int i = nbins; i > 0; --i )
       {
-	if ( obj->GetBinContent(i) != 0 )
-	{
-	   maxRange = TMath::Max(i+1,2);
-	   break;
-	}
+        if ( obj->GetBinContent(i) != 0 )
+        {
+           maxRange = TMath::Max(i+1,2);
+           break;
+        }
       }
 
       obj->GetXaxis()->SetRange(1,maxRange);
