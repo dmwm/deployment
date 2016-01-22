b552ffb9088cca928c89c2977633be4b356ecb69
Andrea Bocci <fwyzard@gmail.com>
2016-01-22 12:10:16 +0100

    ConfDB: fix deployment script. Close #280.

--- confdb/Config.py
+++ confdb/Config.py
@@ -1,9 +1,22 @@
+# -*- coding: utf-8 -*-
+
+import os
+import tempfile
+
+# configuration for ConfDB authentication
 from ConfDBAuth import *
 
+# confguration for CherryPy
 cpconfig = {
   'server.socket_host': '0.0.0.0',
   'server.socket_port': 8340,
   'server.thread_pool': 30
 }
 
+# base URL of the application on the server (e.g. use "/confdb" for "http://cmsweb.cern.ch/confdb"
 base_url = "/confdb"
+
+# local directory with read-write access for temporary files
+state_dir = tempfile.gettempdir()
+if 'STATEDIR' in os.environ:
+    state_dir = os.environ.get('STATEDIR')
