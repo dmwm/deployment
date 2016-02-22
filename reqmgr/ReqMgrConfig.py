fa53a6ca0fa923a0b7f2a0698d86499cacc78db6
Seangchan Ryu <sryu@fnal.gov>
2016-02-22 11:41:02 +0100

    Remove global monitor. Close #289.

--- reqmgr/ReqMgrConfig.py
+++ reqmgr/ReqMgrConfig.py
@@ -1,17 +1,18 @@
-import thread
-thread.stack_size(128*1024)
+__all__ = []
 
+import thread
 import socket
 import re
-import WMCore.HTTPFrontEnd.RequestManager.ReqMgrConfiguration as ReqMgrConfig
-__all__ = []
-from WMCore.WMInit import getWMBASE
 import os.path
+
+import WMCore.HTTPFrontEnd.RequestManager.ReqMgrConfiguration as ReqMgrConfig
+from WMCore.WMInit import getWMBASE
+
+thread.stack_size(128*1024)
 INSTALL = getWMBASE()
 
 HOST = socket.getfqdn().lower()
 COUCH = "https://%s/couchdb" % HOST
-ADD_MONITOR_FLAG = False
 
 if re.match(r"^vocms0(?:13[689]|140|16[135]|30[67]|318)\.cern\.ch$", HOST):
   COUCH = "https://cmsweb.cern.ch/couchdb"
@@ -22,8 +23,7 @@ elif re.match(r"^vocms0127\.cern\.ch$", HOST):
 elif re.match(r"^vocms0126\.cern\.ch$", HOST):
   COUCH = "https://cmsweb-sec.cern.ch/couchdb"
 
-config = ReqMgrConfig.reqMgrConfig(installation=INSTALL,
-  couchurl = COUCH, addMonitor = ADD_MONITOR_FLAG)
+config = ReqMgrConfig.reqMgrConfig(installation=INSTALL, couchurl = COUCH)
 
 TEMPLATES = os.path.normpath(os.path.join(INSTALL, '../../../data/templates/WMCore/WebTools'))
 JAVASCRIPT_PATH = os.path.normpath(os.path.join(INSTALL, '../../../data/javascript'))
@@ -32,13 +32,6 @@ HTML_PATH = os.path.normpath(os.path.join(INSTALL, '../../../data/html'))
 config.webapp_("reqmgr")
 config.reqmgr.html = os.path.join(HTML_PATH, 'RequestManager')
 config.reqmgr.templates = os.path.join(TEMPLATES, 'RequestManager')
-
-if ADD_MONITOR_FLAG:
-    config.reqmgr.views.active.GlobalMonitor.templates = os.path.join(TEMPLATES, 'GlobalMonitor')
-    config.reqmgr.views.active.GlobalMonitor.javascript = JAVASCRIPT_PATH
-    config.reqmgr.views.active.GlobalMonitor.html = HTML_PATH
-    config.reqmgr.views.active.monitorSvc.templates = TEMPLATES
-    config.reqmgr.views.active.monitorSvc.serviceURL = "local"
 
 config.reqmgr.views.active.reqMgr.html = os.path.join(HTML_PATH, 'RequestManager')
 config.reqmgr.views.active.reqMgr.templates = TEMPLATES
@@ -51,4 +44,3 @@ config.SecurityModule.key_file = "%s/auth/wmcore-auth/header-auth-key" % __file_
 config.Webtools.proxy_base = 'True'
 config.Webtools.thread_pool = 30
 config.Webtools.environment = 'production'
-
