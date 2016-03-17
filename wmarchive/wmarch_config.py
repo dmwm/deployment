187bf9568eadfe445ff549808d37e952a00ba585
Valentin Kuznetsov <vkuznet@gmail.com>
2016-03-17 14:57:33 -0400

    Fix long_storage_uri

--- wmarchive/wmarch_config.py
+++ wmarchive/wmarch_config.py
@@ -42,4 +42,4 @@ data.object = 'WMArchive.Service.RestApi.RestInterface'
 # data.short_storage_uri = 'fileio:%s/storage' % ROOTDIR
 #data.short_storage_uri = 'avroio:%s/storage/schema.avsc' % ROOTDIR
 data.short_storage_uri = 'mongodb://localhost:8230'
-data.long_storage_uri = 'hadoop'
+data.long_storage_uri = 'sparkio:///cms/wmarchive/data/schema.avsc'
