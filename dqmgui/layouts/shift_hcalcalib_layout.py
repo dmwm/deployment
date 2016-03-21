d29dd8a8802ab738e86c8ed6a85c8e8133b4fc65
vkhristenko <vdkhristenko1991@gmail.com>
2016-03-21 13:46:40 +0100

    calib for shift and hcal

--- dqmgui/layouts/shift_hcalcalib_layout.py
+++ dqmgui/layouts/shift_hcalcalib_layout.py
@@ -1,3 +1,5 @@
+
+
 # Dummy check of python syntax within file when run stand-alone
 if __name__=="__main__":
     class DQMItem:
@@ -8,49 +10,10 @@ if __name__=="__main__":
 
 def shifthcalcaliblayout(i, p, *rows): i["00 Shift/HcalCalib/" + p] = DQMItem(layout=rows)
 
-# BVB:
-# This shift layout is clearly not up to date anymore with what is currently
-# produced. Since all these plots don't exist and always show up empty and
-# the instructions ask not to look at it, it's better to just simply remove
-# the folder from the shift folder, so that the shifter doesn't have to wonder
-# each time why it's there.
-# So I'm commenting them out now.
-# If one day it is decided to have the online shifters look at HCAL calibration
-# plots again, we can start from here.
+shifthcalcaliblayout(dqmitems, '00 Current Summary', [{'path':'HcalCalib/PedestalTask/Summary/Summary', 'description':"""Calibration Summary. Anything that is not either WHITE or GREEN is BAD.<br>Color Scheme:<br><font color='green'>GOOD</font> for GOOD<br><font color='yellow'>PROBLEMATIC</font> for Problematic<br><font color='red'>BAD</font> for BAD<br><font color='black'>BAD</font> for BAD<br>WHITE color stands for INAPPLICABLE flag<br>Flags(Y) vs FED(X). For details...   <a href="https://twiki.cern.ch/twiki/bin/view/CMS/HcalDQMRun2TaskDescription#Summary_Generation_Description">Details...</a>"""}])
 
-##shifthcalcaliblayout(dqmitems, "01 HcalCalib Summary",
-##           [{ 'path':"HcalCalib/EventInfo/reportSummaryMap",
-##             'description':"This shows the fraction of bad cells in each subdetector.  All subdetectors should appear green.  Values should all be above 98%."}]
-##           )
-##
-##shifthcalcaliblayout(dqmitems, "02 HcalCalib Problem Pedestals",
-##                [{ 'path':"HcalCalib/DetDiagPedestalMonitor_Hcal/ ProblemDetDiagPedestal",
-##                   'description': "Channels with pedestals that have some kind of problem (bad mean or RMS, unstable, or missing).  This histogram should normally be empty."}]
-##                )
-##
-##shifthcalcaliblayout(dqmitems, "03 HcalCalib Problem Laser",
-##                [{ 'path':"HcalCalib/DetDiagLaserMonitor_Hcal/ ProblemDetDiagLaser",
-##                   'description': "Channels that are outside reference bounds in either time or energy.  This histogram should normally be empty."}]
-##)
-##
-##shifthcalcaliblayout(dqmitems, "04 HcalCalib Pedestal Reference Comparison",
-##                [{'path':"HcalCalib/DetDiagPedestalMonitor_Hcal/Summary Plots/HB Pedestal-Reference Distribution (average over 4 caps)",
-##                  'description':"Distribution of calculated pedestals - reference values in the Hcal Barrel detector."},
-##                 {'path':"HcalCalib/DetDiagPedestalMonitor_Hcal/Summary Plots/HE Pedestal-Reference Distribution (average over 4 caps)",
-##                  'description':"Distribution of calculated pedestals - reference values in the Hcal Endcap detector."}],
-##                [{'path':"HcalCalib/DetDiagPedestalMonitor_Hcal/Summary Plots/HF Pedestal-Reference Distribution (average over 4 caps)",
-##                'description':"Distribution of calculated pedestals - reference values in the Hcal Forward detector."},
-##                 {'path':"HcalCalib/DetDiagPedestalMonitor_Hcal/Summary Plots/HO Pedestal-Reference Distribution (average over 4 caps)",
-##                 'description':"Distribution of calculated pedestals - reference values in the Hcal Outer detector."} ]
-##                )
-##
-##shifthcalcaliblayout(dqmitems, "05 HcalCalib Laser RBX Plots",
-##                [{ 'path':"HcalCalib/DetDiagLaserMonitor_Hcal/Summary Plots/HB RBX average Time-Ref",
-##                   'description':"1D Laser RBX energy - reference in HB"},
-##                 { 'path':"HcalCalib/DetDiagLaserMonitor_Hcal/Summary Plots/HE RBX average Time-Ref",
-##                   'description':"1D Laser RBX energy - reference in HE"}],
-##                [{ 'path':"HcalCalib/DetDiagLaserMonitor_Hcal/Summary Plots/HF RoBox average Time-Ref",
-##                   'description':"1D Laser RBX energy - reference in HF"},
-##                 { 'path':"HcalCalib/DetDiagLaserMonitor_Hcal/Summary Plots/HO RBX average Time-Ref",
-##                   'description':"1D Laser RBX energy - reference in HO"}]
-##                )
+shifthcalcaliblayout(dqmitems, 'Pedestal Mean Bad', [{'path':'HcalCalib/PedestalTask/MeanBad/depth/depth1', 'description':"""Bad Pedestal Means w.r.t. CondDB.   <a href="https://twiki.cern.ch/twiki/bin/view/CMS/HcalDQMRun2TaskDescription#Pedestal_Task_Description">Details...</a>"""}, {'path':'HcalCalib/PedestalTask/MeanBad/depth/depth2', 'description':"""Bad Pedestal Means w.r.t. CondDB.   <a href="https://twiki.cern.ch/twiki/bin/view/CMS/HcalDQMRun2TaskDescription#Pedestal_Task_Description">Details...</a>"""}], [{'path':'HcalCalib/PedestalTask/MeanBad/depth/depth3', 'description':"""Bad Pedestal Means w.r.t. CondDB.   <a href="https://twiki.cern.ch/twiki/bin/view/CMS/HcalDQMRun2TaskDescription#Pedestal_Task_Description">Details...</a>"""}, {'path':'HcalCalib/PedestalTask/MeanBad/depth/depth4', 'description':"""Bad Pedestal Means w.r.t. CondDB.   <a href="https://twiki.cern.ch/twiki/bin/view/CMS/HcalDQMRun2TaskDescription#Pedestal_Task_Description">Details...</a>"""}])
+
+shifthcalcaliblayout(dqmitems, 'Pedestal RMS Bad', [{'path':'HcalCalib/PedestalTask/RMSBad/depth/depth1', 'description':"""Bad Pedestal RMS w.r.t. CondDB.   <a href="https://twiki.cern.ch/twiki/bin/view/CMS/HcalDQMRun2TaskDescription#Pedestal_Task_Description">Details...</a>"""}, {'path':'HcalCalib/PedestalTask/RMSBad/depth/depth2', 'description':"""Bad Pedestal RMS w.r.t. CondDB.   <a href="https://twiki.cern.ch/twiki/bin/view/CMS/HcalDQMRun2TaskDescription#Pedestal_Task_Description">Details...</a>"""}], [{'path':'HcalCalib/PedestalTask/RMSBad/depth/depth3', 'description':"""Bad Pedestal RMS w.r.t. CondDB.   <a href="https://twiki.cern.ch/twiki/bin/view/CMS/HcalDQMRun2TaskDescription#Pedestal_Task_Description">Details...</a>"""}, {'path':'HcalCalib/PedestalTask/RMSBad/depth/depth4', 'description':"""Bad Pedestal RMS w.r.t. CondDB.   <a href="https://twiki.cern.ch/twiki/bin/view/CMS/HcalDQMRun2TaskDescription#Pedestal_Task_Description">Details...</a>"""}])
+
+shifthcalcaliblayout(dqmitems, 'Pedestal Missing', [{'path':'HcalCalib/PedestalTask/Missing/depth/depth1', 'description':"""Missing channels w.r.t. CondDB.   <a href="https://twiki.cern.ch/twiki/bin/view/CMS/HcalDQMRun2TaskDescription#Pedestal_Task_Description">Details...</a>"""}, {'path':'HcalCalib/PedestalTask/Missing/depth/depth2', 'description':"""Missing channels w.r.t. CondDB.   <a href="https://twiki.cern.ch/twiki/bin/view/CMS/HcalDQMRun2TaskDescription#Pedestal_Task_Description">Details...</a>"""}], [{'path':'HcalCalib/PedestalTask/Missing/depth/depth3', 'description':"""Missing channels w.r.t. CondDB.   <a href="https://twiki.cern.ch/twiki/bin/view/CMS/HcalDQMRun2TaskDescription#Pedestal_Task_Description">Details...</a>"""}, {'path':'HcalCalib/PedestalTask/Missing/depth/depth4', 'description':"""Missing channels w.r.t. CondDB.   <a href="https://twiki.cern.ch/twiki/bin/view/CMS/HcalDQMRun2TaskDescription#Pedestal_Task_Description">Details...</a>"""}])
