875f54d78f92067e5a75b765bcc315e6023a388b
Javier <Javier.Fernandez@cern.ch>
2016-05-18 21:29:13 +0200

    Fixed tab/space issue

--- dqmgui/layouts/dt-layouts.py
+++ dqmgui/layouts/dt-layouts.py
@@ -114,23 +114,23 @@ for wheel in range(-2, 3):
     for station in range (1, 5):
         for sector in range (1, 15):
             if station != 4 and (sector == 13 or sector == 14):
-                continue
-	    name = "09-Nhits/Wheel" + str(wheel) + "/Sector" + str(sector) + "_Station" + str(station)
-	    histoname = "DT/02-Segments/Wheel" + str(wheel)+ "/Sector" + str(sector) + "/Station" + str(station) + "/h4DSegmNHits_W" + str(wheel) + "_St" + str(station) + "_Sec" + str(sector)
-	    dtlayout(dqmitems, name,[{ 'path': histoname}])
+               continue
+            name = "09-Nhits/Wheel" + str(wheel) + "/Sector" + str(sector) + "_Station" + str(station)
+            histoname = "DT/02-Segments/Wheel" + str(wheel)+ "/Sector" + str(sector) + "/Station" + str(station) + "/h4DSegmNHits_W" + str(wheel) + "_St" + str(station) + "_Sec" + str(sector)
+            dtlayout(dqmitems, name,[{ 'path': histoname}])
 
 #### SEGMENTS RESIDUALS #########################################################################
 for wheel in range(-2, 3):
     for station in range (1, 5):
         for sector in range (1, 15):
             if station != 4 and (sector == 13 or sector == 14):
-                continue
-	    for sl in range (1, 4):
-	    	if sl == 2 and (sector == 13 or sector == 14):
-			continue
-            	name = "10-Segments/Wheel" + str(wheel) + "/Sector" + str(sector) + "_Station" + str(station) + "_SL" + str(sl)
-            	histoname = "DT/02-Segments/Wheel" + str(wheel)+ "/Sector" + str(sector) + "/Station" + str(station) + "/hResDist_W" + str(wheel) + "_St" + str(station) + "_Sec" + str(sector) + "_SL" + str(sl)
-            	dtlayout(dqmitems, name,[{ 'path': histoname}])
+               continue
+            for sl in range (1, 4):
+                if sl == 2 and (sector == 13 or sector == 14):
+                   continue
+                name = "10-Segments/Wheel" + str(wheel) + "/Sector" + str(sector) + "_Station" + str(station) + "_SL" + str(sl)
+                histoname = "DT/02-Segments/Wheel" + str(wheel)+ "/Sector" + str(sector) + "/Station" + str(station) + "/hResDist_W" + str(wheel) + "_St" + str(station) + "_Sec" + str(sector) + "_SL" + str(sl)
+                dtlayout(dqmitems, name,[{ 'path': histoname}])
 
 #
 #
