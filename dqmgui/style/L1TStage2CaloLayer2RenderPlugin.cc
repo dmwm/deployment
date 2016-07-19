ac663e5ca5911b396828f4b608e05a220d03f687
esmaeel <esi.esk@gmail.com>
2016-07-19 11:01:41 +0200

    CaloLayer2 render-plugin is added now

---
+++ dqmgui/style/L1TStage2CaloLayer2RenderPlugin.cc
@@ -0,0 +1,91 @@
+#include <cassert>
+#include <string>
+
+#include "DQM/DQMRenderPlugin.h"
+
+#include "TCanvas.h"
+#include "TColor.h"
+#include "TH1F.h"
+#include "TH2F.h"
+#include "TStyle.h"
+
+class L1TStage2CaloLayer2RenderPlugin : public DQMRenderPlugin {
+
+ public:
+
+  virtual bool applies(const VisDQMObject& o, const VisDQMImgInfo&) {
+    if (o.name.find("L1T2016/L1TStage2CaloLayer2/") != std::string::npos)
+      return true;
+
+    return false;
+  }
+
+  virtual void preDraw(TCanvas* c, const VisDQMObject& o, const VisDQMImgInfo&, VisDQMRenderInfo&) {
+    if (dynamic_cast<TH1F*>(o.object)) {
+      preDrawTH1F(c, o);
+    } else if (dynamic_cast<TH2F*>(o.object)) {
+      preDrawTH2F(c, o);
+    }
+  }
+
+  virtual void postDraw(TCanvas* c, const VisDQMObject& o, const VisDQMImgInfo&) {
+    if (dynamic_cast<TH1F*>(o.object)) {
+      postDrawTH1F(c, o);
+    } else if (dynamic_cast<TH2F*>(o.object)) {
+      postDrawTH2F(c, o);
+    }
+  }
+
+ private:
+
+  void preDrawTH1F(TCanvas*, const VisDQMObject& o) {
+    TH1F* obj = dynamic_cast<TH1F*>(o.object);
+    assert(obj);
+
+    if (o.name.find("Energy-Sums/EtSumBxOcc") != std::string::npos) {
+      gPad->SetLogy(1);
+    }
+    if (o.name.find("Energy-Sums/METRank") != std::string::npos) {
+      gPad->SetLogy(1);
+    }
+    if (o.name.find("Energy-Sums/METPhi") != std::string::npos) {
+      gPad->SetLogy(1);
+    }
+    if (o.name.find("Energy-Sums/ETTRank") != std::string::npos) {
+      gPad->SetLogy(1);
+    }
+    if (o.name.find("Energy-Sums/MHTRank") != std::string::npos) {
+      gPad->SetLogy(1);
+    }
+    if (o.name.find("Energy-Sums/MHTPhi") != std::string::npos) {
+      gPad->SetLogy(1);
+    }
+    if (o.name.find("Energy-Sums/HTTRank") != std::string::npos) {
+      gPad->SetLogy(1);
+    }
+
+  }
+
+  void preDrawTH2F(TCanvas*, const VisDQMObject& o) {
+    TH2F* obj = dynamic_cast<TH2F*>(o.object);
+    assert(obj);
+
+    obj->SetOption("colz");
+  }
+
+  void postDrawTH1F(TCanvas*, const VisDQMObject& o) {
+    TH1F* obj = dynamic_cast<TH1F*>(o.object);
+    assert(obj);
+
+    gStyle->SetOptStat(111110);
+  }
+
+  void postDrawTH2F(TCanvas*, const VisDQMObject& o) {
+    TH2F* obj = dynamic_cast<TH2F*>(o.object);
+    assert(obj);
+
+    gStyle->SetOptStat(10);
+  }
+};
+
+static L1TStage2CaloLayer2RenderPlugin instance;
