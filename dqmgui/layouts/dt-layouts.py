d1c87baa96217367e0f4b018e0b376d4bfc07e87
Javier <Javier.Fernandez@cern.ch>
2016-07-20 11:50:20 +0200

    Dtdqm qual phi layout. Close #351.

--- dqmgui/layouts/dt-layouts.py
+++ dqmgui/layouts/dt-layouts.py
@@ -132,6 +132,14 @@ for wheel in range(-2, 3):
                 histoname = "DT/02-Segments/Wheel" + str(wheel)+ "/Sector" + str(sector) + "/Station" + str(station) + "/hResDist_W" + str(wheel) + "_St" + str(station) + "_Sec" + str(sector) + "_SL" + str(sl)
                 dtlayout(dqmitems, name,[{ 'path': histoname}])
 
+#### QUALITY vs PHI RAD ############################################################################
+for wheel in range(-2, 3):
+    for station in range (1, 5):
+        for sector in range (1, 13):
+            name = "11-QualityPhi/Wheel" + str(wheel) + "/Sector" + str(sector) + "_Station" + str(station)
+            histoname = "DT/03-LocalTrigger-TM/Wheel" + str(wheel)+ "/Sector" + str(sector) + "/Station" + str(station) + "/LocalTriggerPhi/TM_QualvsPhirad_W" + str(wheel) + "_Sec" + str(sector) + "_St" + str(station)
+            dtlayout(dqmitems, name,[{ 'path': histoname}])
+
 #
 #
 #
