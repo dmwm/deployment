298979e0c3fab1ce55396ad15d3ac8fb8a05bf27
Marcel <marcel.andre.schneider@cern.ch>
2016-11-16 22:25:02 +0100

    Add some features after timing scan experience.
    - User-Selectable range on y-axis
    - User-Selectable range on z(LS)-axis
    - Skip white color index
    - larger legend

--- dqmgui/style/SiPixelPhase1OnlineRenderPlugin.cc
+++ dqmgui/style/SiPixelPhase1OnlineRenderPlugin.cc
@@ -22,7 +22,7 @@ class SiPixelPhase1OnlineRenderPlugin : public DQMRenderPlugin
 public:
   virtual bool applies( const VisDQMObject & o, const VisDQMImgInfo & )
     {
-      if ((o.name.find( "PixelPhase1/" ) != std::string::npos )
+      if ((o.name.find( "PixelPhase1/" ) != std::string::npos || o.name.find( "PixelPilot/" ) != std::string::npos  )
         && (std::string(o.object->GetName()).find( "OnlineBlock" ) != std::string::npos )) {
         return true;
       } else {
@@ -41,7 +41,7 @@ public:
       renderInfo.drawOptions = "AXIS";
     }
 
-  virtual void postDraw( TCanvas *canvas, const VisDQMObject &o, const VisDQMImgInfo & )
+  virtual void postDraw( TCanvas *canvas, const VisDQMObject &o, const VisDQMImgInfo &ri )
     {
       canvas->cd();
       TH2* obj = dynamic_cast<TH2*>( o.object );
@@ -50,7 +50,7 @@ public:
       // Axis range is not shown anywhere, min abused for block size
       int blocksize = int(obj->GetYaxis()->GetXmin()+0.5);
 
-      TLegend* leg = new TLegend(0.75,0.5,0.9,0.95);
+      TLegend* leg = new TLegend(0.75,0.3,0.9,0.95);
       leg->SetHeader("LS Range");
       leg->SetBit(kCanDelete);
 
@@ -59,20 +59,30 @@ public:
 
       int n_color = 0;
 
-      for (int i = 1; i <= obj->GetNbinsY(); i++) {
+      int lower = std::isnan(ri.zaxis.min) ? 1 : int(ri.zaxis.min/blocksize+1);
+      int upper = std::isnan(ri.zaxis.max) ? obj->GetNbinsY() : int(ri.zaxis.max/blocksize);
+      if (lower < 1 || lower > obj->GetNbinsY()) lower = 1;
+      if (upper < 1 || upper > obj->GetNbinsY()) upper = obj->GetNbinsY();
+
+      for (int i = lower; i <= upper; i++) {
         auto name = std::string(obj->GetName()) + "_" + std::to_string(i);
         TH1* h = new TH1F(name.c_str(), "", obj->GetNbinsX(),
                         obj->GetXaxis()->GetXmin(), obj->GetXaxis()->GetXmax());
         h->SetBit(kCanDelete);
         double entries = 0;
+        int nonzerobins = 0;
         for (int x = 1; x < obj->GetNbinsX(); x++) {
+          if (obj->GetBinContent(x, i) > 0) nonzerobins++;
           entries += obj->GetBinContent(x, i);
           h->SetBinContent(x, obj->GetBinContent(x, i));
         }
-        if (entries == 0) continue;
+
+        // suppress single-bin distributions for non-zero-suppressed NDigis etc.
+        if (nonzerobins <= 1) continue;
         h->Scale(ref/entries);
 
         n_color++;
+        if (n_color == 10) n_color = 15; // skip low saturation stuff in between.
         
         h->SetLineColor(n_color);
         h->Draw("SAME");
@@ -81,7 +91,11 @@ public:
         max = std::max(max, h->GetMaximum());
       }
 
-      obj->GetYaxis()->Set(1, 0, max*1.05);
+      double ymin = std::isnan(ri.yaxis.min) ? 0 : ri.yaxis.min;
+      double ymax = std::isnan(ri.yaxis.max) ? max*1.05 : ri.yaxis.max;
+
+      obj->GetYaxis()->Set(1, ymin, ymax);
+      obj->GetYaxis()->SetRange(0,0); // ROOT magic: unset range, use binning min/max set above
       obj->GetYaxis()->SetTitle("");
       leg->Draw();
     }
