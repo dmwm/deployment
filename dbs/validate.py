cc750a6c51ea4940d2e1c99cd509472e413987ed
Yuyi Guo <yuyi@fnal.gov>
2017-02-13 08:47:52 -0600

    add new validation to view and update manage (#439)

--- dbs/validate.py
+++ dbs/validate.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import sys, json, re
-
+if len(sys.argv) < 2:
+  print >> sys.stderr, "Must have one of the input arguments: def, reader, writer or migration "
+  sys.exit(1)
 try:
   d=json.load(sys.stdin)
 except:
@@ -14,9 +16,22 @@ for v in d.values():
   if not isinstance(v,dict):
     print >> sys.stderr, "'%s' is not a viewname -> list of instances map." % v
     sys.exit(1)
-  if set(v.keys()) != set(["DBSReader", "DBSWriter", "DBSMigrate"]):
-    print >> sys.stderr, "Must have view names for 'DBSReader', 'DBSWriter' and 'DBSMigrate', nothing else."
-    sys.exit(1)
+  if sys.argv[1] == "reader":
+    if set(v.keys()) != set(["DBSReader"]):
+      print >> sys.stderr, "Must have view names for 'DBSReader', nothing else."
+      sys.exit(1)
+  elif sys.argv[1] == "writer":
+    if set(v.keys()) != set(["DBSWriter"]):
+      print >> sys.stderr, "Must have view names for 'DBSWriter' , nothing else."
+      sys.exit(1)
+  elif sys.argv[1] == "reader&writer":
+    if set(v.keys()) != set(["DBSReader", "DBSWriter"]):
+      print >> sys.stderr, "Must have view names for 'DBSReader'and 'DBSWriter', nothing else."
+      sys.exit(1)
+  else:
+    if set(v.keys()) != set(["DBSReader", "DBSWriter", "DBSMigrate"]):
+      print >> sys.stderr, "Must have view names for 'DBSReader', 'DBSWriter' and 'DBSMigrate', nothing else."
+      sys.exit(1)
   for t in v.values():
     if not isinstance(t,list):
       print >> sys.stderr, "'%s' is not a list of instance names." % t
@@ -25,5 +40,4 @@ for v in d.values():
       if not re.match('^(prod|int|dev)/(global|phys0[123]|test)$', i):
         print >> sys.stderr, "'%s' is an invalid instance name." % i
         sys.exit(1)
-
 sys.exit(0)
