f3c10057d34e8c13536e5797c935732a4d8a305e
Francesco Fiori <francesco.fiori@cern.ch>
2017-03-29 14:27:52 +0200

    adding superimpesed lines to Tracking maps

--- dqmgui/style/SiPixelMapsRenderPlugin.cc
+++ dqmgui/style/SiPixelMapsRenderPlugin.cc
@@ -31,7 +31,7 @@ class SiPixelMapsRenderPlugin : public DQMRenderPlugin
 public:
   virtual bool applies( const VisDQMObject & o, const VisDQMImgInfo & )
     {
-      if (o.name.find( "PixelPhase1/Phase1_MechanicalView" ) != std::string::npos || o.name.find( "PixelPilot/" ) != std::string::npos ){
+      if (o.name.find( "PixelPhase1/Phase1_MechanicalView" ) != std::string::npos || o.name.find( "PixelPhase1/Tracks" ) != std::string::npos || o.name.find( "PixelPilot/" ) != std::string::npos ){
 	  if (o.object && o.name.find( "Coord" ) != std::string::npos ) return true;
 	  return false;
 	}
