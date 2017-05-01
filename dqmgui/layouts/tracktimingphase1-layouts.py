db56801c34ba89e56fbb8adef865ac1280fd257f
enochnotsocool <enochnotsocool@gmail.com>
2017-05-01 21:20:05 +0200

    Text Fixes for axis

--- dqmgui/layouts/tracktimingphase1-layouts.py
+++ dqmgui/layouts/tracktimingphase1-layouts.py
@@ -2,93 +2,93 @@ def pixellayout(i, p, *rows): i["TrackTimingPixelPhase1/Layouts/" + p] = DQMItem
 
 
 ### Digis
-pixellayout(dqmitems, "01a - Timing_Digi_Barrel",
+pixellayout(dqmitems, "01a - digi_Barrel",
    [{ 'path': "TrackTimingPixelPhase1/Phase1_MechanicalView/num_digis_per_OnlineBlock_PXBarrel",
       'description': "Mean adc value per lumisection",
       'draw': { 'withref': "no" }}
       ] )
 
-pixellayout(dqmitems, "01b - Timing_Digi_Forward",
+pixellayout(dqmitems, "01b - digi_Forward",
    [{ 'path': "TrackTimingPixelPhase1/Phase1_MechanicalView/num_digis_per_OnlineBlock_PXForward",
       'description': "Mean adc value per lumisection",
       'draw': { 'withref': "no" }}] )
 
 ### ADC
-pixellayout(dqmitems, "02a - Timing_ADC_Barrel",
+pixellayout(dqmitems, "02a - ADC_Barrel",
     [{ 'path': "TrackTimingPixelPhase1/Phase1_MechanicalView/adc_per_OnlineBlock_PXBarrel",
       'description': "Mean adc value per lumisection",
       'draw': { 'withref': "no" }}])
-pixellayout(dqmitems, "02b - Timing_ADC_Barrel",
+pixellayout(dqmitems, "02b - ADC_Forward",
     [{ 'path': "TrackTimingPixelPhase1/Phase1_MechanicalView/adc_per_OnlineBlock_PXForward",
       'description': "Mean adc value per lumisection",
       'draw': { 'withref': "no" }}])
 
 ### NumCluster
-pixellayout(dqmitems, "03a - Timing_Cluster_Number_Barrel",
+pixellayout(dqmitems, "03a - num_cluster_Barrel",
    [{ 'path': "TrackTimingPixelPhase1/Phase1_MechanicalView/num_clusters_per_OnlineBlock_PXBarrel",
       'description': "Mean cluster value per lumisection in barrel",
       'draw': { 'withref': "no" }}] )
-pixellayout(dqmitems, "03b - Timing_Cluster_Number_Forward",
+pixellayout(dqmitems, "03b - num_cluster_Forward",
     [{ 'path': "TrackTimingPixelPhase1/Phase1_MechanicalView/num_clusters_per_OnlineBlock_PXForward",
       'description': "Mean cluster value per lumisection in endcap",
       'draw': { 'withref': "no" }}])
 
 ### Cluster Charge
-pixellayout(dqmitems, "04a - Timing_Cluster_Charge_Barrel",
+pixellayout(dqmitems, "04a - charge_Barrel",
   [{ 'path': "TrackTimingPixelPhase1/Phase1_MechanicalView/charge_per_OnlineBlock_PXBarrel",
       'description': "Mean cluster charge per lumisection in barrel",
       'draw': { 'withref': "no" }} ] )
-pixellayout(dqmitems, "04b - Timing_Cluster_Charge_Forward",
+pixellayout(dqmitems, "04b - charge_Forward",
     [{ 'path': "TrackTimingPixelPhase1/Phase1_MechanicalView/charge_per_OnlineBlock_PXForward",
       'description': "Mean cluster charge per lumisection in barrel",
       'draw': { 'withref': "no" }}] )
 
 ### Size
-pixellayout(dqmitems, "05a - Timing_size_Forward",
+pixellayout(dqmitems, "05a - size_Barrel",
     [{ 'path': "TrackTimingPixelPhase1/Phase1_MechanicalView/size_per_OnlineBlock_PXBarrel",
       'description': "Mean cluster size per lumisection in barrel",
       'draw': { 'withref': "no" }}])
-pixellayout(dqmitems, "05b - Timing_size_Forward",
+pixellayout(dqmitems, "05b - size_Forward",
     [{ 'path': "TrackTimingPixelPhase1/Phase1_MechanicalView/size_per_OnlineBlock_PXForward",
       'description': "Mean cluster size per lumisection in barrel",
       'draw': { 'withref': "no" }}] )
 
 ### Charge On track
-pixellayout(dqmitems, "06a - Timing_charge_ontrack_Barrel",
+pixellayout(dqmitems, "06a - charge_ontrack_Barrel",
     [{ 'path': "TrackTimingPixelPhase1/Phase1_Track/charge_per_OnlineBlock_PXBarrel",
       'description': "Mean cluster size per lumisection in barrel",
       'draw': { 'withref': "no" }}])
-pixellayout(dqmitems, "06b - Timing_charge_ontrack_Forward",
+pixellayout(dqmitems, "06b - charge_ontrack_Forward",
     [{ 'path': "TrackTimingPixelPhase1/Phase1_Track/charge_per_OnlineBlock_PXForward",
       'description': "Mean cluster size per lumisection in barrel",
       'draw': { 'withref': "no" }}] )
 
 ### Number of cluster on track
-pixellayout(dqmitems, "07a - Timing_num_cluster_ontrack_Barrel",
+pixellayout(dqmitems, "07a - num_cluster_ontrack_Barrel",
     [{ 'path': "TrackTimingPixelPhase1/Phase1_Track/num_clusters_ontrack_per_OnlineBlock_PXBarrel",
       'description': "Mean cluster size per lumisection in barrel",
       'draw': { 'withref': "no" }}])
-pixellayout(dqmitems, "07b - Timing_num_cluster_ontrack_Forward",
+pixellayout(dqmitems, "07b - num_cluster_ontrack_Forward",
     [{ 'path': "TrackTimingPixelPhase1/Phase1_Track/num_clusters_ontrack_per_OnlineBlock_PXForward",
       'description': "Mean cluster size per lumisection in barrel",
       'draw': { 'withref': "no" }}] )
 
 ### Residualx
-pixellayout(dqmitems, "08a - Timing_residualx_Barrel",
+pixellayout(dqmitems, "08a - residualx_Barrel",
     [{ 'path': "TrackTimingPixelPhase1/Phase1_Track/residual_x_per_OnlineBlock_PXBarrel",
       'description': "Mean cluster size per lumisection in barrel",
       'draw': { 'withref': "no" }}])
-pixellayout(dqmitems, "08b - Timing_residualx_Forward",
+pixellayout(dqmitems, "08b - residualx_Forward",
     [{ 'path': "TrackTimingPixelPhase1/Phase1_Track/residual_x_per_OnlineBlock_PXForward",
       'description': "Mean cluster size per lumisection in barrel",
       'draw': { 'withref': "no" }}] )
 
 ### Residualy
-pixellayout(dqmitems, "09a - Timing_residualy_Barrel",
+pixellayout(dqmitems, "09a - residualy_Barrel",
     [{ 'path': "TrackTimingPixelPhase1/Phase1_Track/residual_y_per_OnlineBlock_PXBarrel",
       'description': "Mean cluster size per lumisection in barrel",
       'draw': { 'withref': "no" }}])
-pixellayout(dqmitems, "09b - Timing_residualy_Forward",
+pixellayout(dqmitems, "09b - residualy_Forward",
     [{ 'path': "TrackTimingPixelPhase1/Phase1_Track/residual_y_per_OnlineBlock_PXForward",
       'description': "Mean cluster size per lumisection in barrel",
       'draw': { 'withref': "no" }}] )
@@ -96,60 +96,60 @@ pixellayout(dqmitems, "09b - Timing_residualy_Forward",
 ############################## NON ONLINEBLOCK ##############################################
 
 ### ADC
-pixellayout(dqmitems, "11a - Timing_ADC_Barrel",
+pixellayout(dqmitems, "11a - ADC_Barrel",
     [{ 'path': "TrackTimingPixelPhase1/Phase1_MechanicalView/adc_PXBarrel",
       'description': "Mean adc value per lumisection",
       'draw': { 'withref': "no" }}])
-pixellayout(dqmitems, "11b - Timing_ADC_Barrel",
+pixellayout(dqmitems, "11b - ADC_Forward",
     [{ 'path': "TrackTimingPixelPhase1/Phase1_MechanicalView/adc_PXForward",
       'description': "Mean adc value per lumisection",
       'draw': { 'withref': "no" }}])
 ### Cluster Charge
-pixellayout(dqmitems, "12a - Timing_Cluster_Charge_Barrel",
+pixellayout(dqmitems, "12a - charge_Barrel",
   [{ 'path': "TrackTimingPixelPhase1/Phase1_MechanicalView/charge_PXBarrel",
       'description': "Mean cluster charge per lumisection in barrel",
       'draw': { 'withref': "no" }} ] )
-pixellayout(dqmitems, "12b - Timing_Cluster_Charge_Forward",
+pixellayout(dqmitems, "12b - charge_Forward",
     [{ 'path': "TrackTimingPixelPhase1/Phase1_MechanicalView/charge_PXForward",
       'description': "Mean cluster charge per lumisection in barrel",
       'draw': { 'withref': "no" }}] )
 
 ### Size
-pixellayout(dqmitems, "13a - Timing_size_Forward",
+pixellayout(dqmitems, "13a - size_Barrel",
     [{ 'path': "TrackTimingPixelPhase1/Phase1_MechanicalView/size_PXBarrel",
       'description': "Mean cluster size per lumisection in barrel",
       'draw': { 'withref': "no" }}])
-pixellayout(dqmitems, "13b - Timing_size_Forward",
+pixellayout(dqmitems, "13b - size_Forward",
     [{ 'path': "TrackTimingPixelPhase1/Phase1_MechanicalView/size_PXForward",
       'description': "Mean cluster size per lumisection in barrel",
       'draw': { 'withref': "no" }}] )
 
 ### Charge On track
-pixellayout(dqmitems, "14a - Timing_charge_ontrack_Barrel",
+pixellayout(dqmitems, "14a - charge_ontrack_Barrel",
     [{ 'path': "TrackTimingPixelPhase1/Phase1_Track/charge_PXBarrel",
       'description': "Mean cluster size per lumisection in barrel",
       'draw': { 'withref': "no" }}])
-pixellayout(dqmitems, "14b - Timing_charge_ontrack_Forward",
+pixellayout(dqmitems, "14b - charge_ontrack_Forward",
     [{ 'path': "TrackTimingPixelPhase1/Phase1_Track/charge_PXForward",
       'description': "Mean cluster size per lumisection in barrel",
       'draw': { 'withref': "no" }}] )
 
 ### Residualx
-pixellayout(dqmitems, "15a - Timing_residualx_Barrel",
+pixellayout(dqmitems, "15a - residualx_Barrel",
     [{ 'path': "TrackTimingPixelPhase1/Phase1_Track/residual_x_PXBarrel",
       'description': "Mean cluster size per lumisection in barrel",
       'draw': { 'withref': "no" }}])
-pixellayout(dqmitems, "15b - Timing_residualx_Forward",
+pixellayout(dqmitems, "15b - residualx_Forward",
     [{ 'path': "TrackTimingPixelPhase1/Phase1_Track/residual_x_PXForward",
       'description': "Mean cluster size per lumisection in barrel",
       'draw': { 'withref': "no" }}] )
 
 ### Residualy
-pixellayout(dqmitems, "16a - Timing_residualy_Barrel",
+pixellayout(dqmitems, "16a - residualy_Barrel",
     [{ 'path': "TrackTimingPixelPhase1/Phase1_Track/residual_y_PXBarrel",
       'description': "Mean cluster size per lumisection in barrel",
       'draw': { 'withref': "no" }}])
-pixellayout(dqmitems, "16b - Timing_residualy_Forward",
+pixellayout(dqmitems, "16b - residualy_Forward",
     [{ 'path': "TrackTimingPixelPhase1/Phase1_Track/residual_y_PXForward",
       'description': "Mean cluster size per lumisection in barrel",
       'draw': { 'withref': "no" }}] )
--- dqmgui/style/SiPixelRenderPlugin.cc
+++ dqmgui/style/SiPixelRenderPlugin.cc
@@ -149,7 +149,7 @@ private:
     }
   };
 
-  void draw_line(double x1, double x2, double y1, double y2 ) 
+  void draw_line(double x1, double x2, double y1, double y2 )
     {
       TLine* l = new TLine(x1, y1, x2, y2);
       l->SetBit(kCanDelete);
@@ -267,7 +267,7 @@ private:
           obj->SetOption("colztext");
           if( obj->GetEntries() > 0. ) gPad->SetLogz(1);
         }
-        
+
       if( o.name.find( "nerrors_per_type" ) != std::string::npos ) // FED error plots
         {
           gPad->SetBottomMargin(0.20);
@@ -276,21 +276,22 @@ private:
           obj->GetXaxis()->SetBinLabel(25+1-25, "ROC of 25"           );
           obj->GetXaxis()->SetBinLabel(26+1-25, "Gap word"            );
           obj->GetXaxis()->SetBinLabel(27+1-25, "Dummy word"          );
-          obj->GetXaxis()->SetBinLabel(28+1-25, "FIFO full"           );   
+          obj->GetXaxis()->SetBinLabel(28+1-25, "FIFO full"           );
           obj->GetXaxis()->SetBinLabel(29+1-25, "Timeout"             );
-          obj->GetXaxis()->SetBinLabel(30+1-25, "TBM error trailer"   );  
+          obj->GetXaxis()->SetBinLabel(30+1-25, "TBM error trailer"   );
           obj->GetXaxis()->SetBinLabel(31+1-25, "TBM/FED mismatch"    );
           obj->GetXaxis()->SetBinLabel(32+1-25, "Slink Header"        );
           obj->GetXaxis()->SetBinLabel(33+1-25, "Slink Trailer"       );
           obj->GetXaxis()->SetBinLabel(34+1-25, "Event size"          );
           obj->GetXaxis()->SetBinLabel(35+1-25, "FED channel number"  );
-          obj->GetXaxis()->SetBinLabel(36+1-25, "ROC value"           ); 
+          obj->GetXaxis()->SetBinLabel(36+1-25, "ROC value"           );
           obj->GetXaxis()->SetBinLabel(37+1-25, "dcol or pixel value" );
           obj->GetXaxis()->SetBinLabel(38+1-25, "Readout order"       );
           obj->GetXaxis()->SetBinLabel(39+1-25, "CRC error"           );
           obj->GetXaxis()->SetBinLabel(40+1-25, "overflow"            );
+          obj->GetXaxis()->SetTitle("");
         }
-      
+
       if( o.name.find( "avgfedDigiOccvsLumi" ) != std::string::npos )
         {
           obj->SetOption("colz");
@@ -343,11 +344,11 @@ private:
       ya->SetTitleSize(0.04);
       ya->SetLabelSize(0.03);
       TGaxis::SetMaxDigits(3);
-      
+
       if( o.name.find("digi_occupancy_per_col_per_row") != std::string::npos ){
          // Horizontal
          draw_line(0,416,80,80);
-        
+
          // Vertical
          draw_line(52 , 52,0,160);
          draw_line(104,104,0,160);
@@ -420,7 +421,7 @@ private:
       if( o.name.find( "SUMOFF_charge_OnTrack_Endcap" ) != std::string::npos ){ obj->SetMinimum(-5.); obj->SetMaximum(45.); }
       if( o.name.find( "SUMOFF_nclusters_OnTrack_Endcap" ) != std::string::npos ){ obj->SetMinimum(-0.1); obj->SetMaximum(2.5); }
       if( o.name.find( "SUMOFF_size_OnTrack_Endcap" ) != std::string::npos ){ obj->SetMinimum(-0.1); obj->SetMaximum(4.); }
-      
+
       if( o.name.find( "tbmmessage_FED" ) != std::string::npos )
         {
           gPad->SetBottomMargin(0.20);
@@ -433,6 +434,7 @@ private:
           obj->GetXaxis()->SetBinLabel( 5+1, "Reset ROC"            );
           obj->GetXaxis()->SetBinLabel( 6+1, "Reset TBM"            );
           obj->GetXaxis()->SetBinLabel( 7+1, "No token bit pass"    );
+          obj->GetXaxis()->SetTitle("");
         }
       if( o.name.find("tbmtype_FED") != std::string::npos )
         {
@@ -443,6 +445,31 @@ private:
           obj->GetXaxis()->SetBinLabel( 2+1, "FSM error"           );
           obj->GetXaxis()->SetBinLabel( 3+1, "Invalid num of ROCs" );
           obj->GetXaxis()->SetBinLabel( 4+1, "Multiple messages"   );
+          obj->GetXaxis()->SetTitle("");
+        }
+
+      if( o.name.find( "nerrors_per_type" ) != std::string::npos ) // FED error plots
+        {
+          gPad->SetBottomMargin(0.20);
+          // +1 since first bin is indexed as bin 1 (not bin 0)
+          // -25 since bin begins at 25
+          obj->GetXaxis()->SetBinLabel(25+1-25, "ROC of 25"           );
+          obj->GetXaxis()->SetBinLabel(26+1-25, "Gap word"            );
+          obj->GetXaxis()->SetBinLabel(27+1-25, "Dummy word"          );
+          obj->GetXaxis()->SetBinLabel(28+1-25, "FIFO full"           );
+          obj->GetXaxis()->SetBinLabel(29+1-25, "Timeout"             );
+          obj->GetXaxis()->SetBinLabel(30+1-25, "TBM error trailer"   );
+          obj->GetXaxis()->SetBinLabel(31+1-25, "TBM/FED mismatch"    );
+          obj->GetXaxis()->SetBinLabel(32+1-25, "Slink Header"        );
+          obj->GetXaxis()->SetBinLabel(33+1-25, "Slink Trailer"       );
+          obj->GetXaxis()->SetBinLabel(34+1-25, "Event size"          );
+          obj->GetXaxis()->SetBinLabel(35+1-25, "FED channel number"  );
+          obj->GetXaxis()->SetBinLabel(36+1-25, "ROC value"           );
+          obj->GetXaxis()->SetBinLabel(37+1-25, "dcol or pixel value" );
+          obj->GetXaxis()->SetBinLabel(38+1-25, "Readout order"       );
+          obj->GetXaxis()->SetBinLabel(39+1-25, "CRC error"           );
+          obj->GetXaxis()->SetBinLabel(40+1-25, "overflow"            );
+          obj->GetXaxis()->SetTitle("");
         }
     }
 
