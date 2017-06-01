20bdc9bb4d8fa10c75e787e6310fa8b4bd3ea278
Esmaeel Eskandari <esi.esk@gmail.com>
2017-06-01 19:24:09 +0200

    L1TEMU shifter space is updated

--- dqmgui/layouts/shift_l1temulator_layout.py
+++ dqmgui/layouts/shift_l1temulator_layout.py
@@ -1,13 +1,13 @@
 def l1temulayout(i, p, *rows): i["00 Shift/L1TEMU/" + p] = DQMItem(layout=rows)
 
-l1temulayout(dqmitems,"00 - uGMT summary - data vs emulator comparison",
-             [{'path': "L1TEMU/L1TdeStage2uGMT/data_vs_emulator_comparison/summary", 'description': "uGMT summary - data vs emulator comparison . For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftTriggerEmulator\">here</a>."}])
+l1temulayout(dqmitems,"00 - uGMT - data vs emulator misMatch ratio",
+             [{'path': "L1TEMU/L1TdeStage2uGMT/data_vs_emulator_comparison/mismatchRatio", 'description': "uGMT - data vs emulator misMatch ratio. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftTriggerEmulator\">here</a>."}])
 
-l1temulayout(dqmitems,"01 - BMTF summary - data vs emulator comparison",
-             [{'path': "L1TEMU/L1TdeStage2BMTF/summary", 'description': "BMTF summary - data vs emulator comparison . For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftTriggerEmulator\">here</a>."}])
+l1temulayout(dqmitems,"01 - BMTF - data vs emulator misMatch ratio",
+             [{'path': "L1TEMU/L1TdeStage2BMTF/mismatchRatio", 'description': "BMTF - data vs emulator misMatch ratio. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftTriggerEmulator\">here</a>."}])
 
-l1temulayout(dqmitems,"02 - OMTF summary - data vs emulator comparison",
-             [{'path': "L1TEMU/L1TdeStage2OMTF/summary", 'description': "OMTF summary - data vs emulator comparison . For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftTriggerEmulator\">here</a>."}])
+l1temulayout(dqmitems,"02 - OMTF - data vs emulator misMatch ratio",
+             [{'path': "L1TEMU/L1TdeStage2OMTF/mismatchRatio", 'description': "OMTF - data vs emulator misMatch ratio. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftTriggerEmulator\">here</a>."}])
 
-l1temulayout(dqmitems,"03 - EMTF summary - data vs emulator comparison",
-             [{'path': "L1TEMU/L1TdeStage2EMTF/summary", 'description': "EMTF summary - data vs emulator comparison . For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftTriggerEmulator\">here</a>."}])
+l1temulayout(dqmitems,"03 - EMTF - data vs emulator misMatch ratio",
+             [{'path': "L1TEMU/L1TdeStage2EMTF/mismatchRatio", 'description': "EMTF - data vs emulator misMatch ratio. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftTriggerEmulator\">here</a>."}])
