bdc714782e37bb7022600ef0fb4bfd7fd85964a5
Fabrizio Ferro <fabferro@users.noreply.github.com>
2017-06-23 12:29:42 +0200

    Recovering CTPPS Pixel layouts (#502)
    * recovering ctpps pixel layouts
    
    * recovering ctpps pixel layouts for T0
    
    * fix indentation
    
    * rebasing
    
    * rebasing

--- dqmgui/layouts/ctpps_T0_layouts.py
+++ dqmgui/layouts/ctpps_T0_layouts.py
@@ -1,10 +1,15 @@
 def CTPPSTrackingStripLayout(i, p, *rows): i["CTPPS/TrackingStrip/Layouts/" + p] = DQMItem(layout=rows)
 def CTPPSTimingDiamondLayout(i, p, *rows): i["CTPPS/TimingDiamond/Layouts/" + p] = DQMItem(layout=rows)
+def CTPPSTrackingPixelLayout(i, p, *rows): i["CTPPS/TrackingPixel/Layouts/" + p] = DQMItem(layout=rows)
 
 strips_stations = [ "sector 45/station 210", "sector 56/station 210" ]
 strips_units = [ "nr", "fr" ]
 
 diamond_stations = [ "sector 45/station 220cyl/cyl_hr", "sector 56/station 220cyl/cyl_hr" ]
+
+pixstations = [ "sector 45/station 220/", "sector 56/station 220/" ]
+planes  = [ "0","1","2" ]
+planes2 = [ "3","4","5" ]
 
 # layouts with no overlays
 for plot in [ "active planes", "activity in planes (2D)", "vfats with any problem", "track XY profile" ]:
@@ -66,3 +71,40 @@ for station in diamond_stations:
   rows.append(row)
 
   CTPPSTimingDiamondLayout(dqmitems, "clocks edges", *rows)
+###
+#####   CTPPS Pixel Layouts
+###
+
+for plot in ["planes activity", "hit multiplicity in planes","hit average multiplicity in planes"]:
+  rows = list()
+  for station in pixstations:
+    row = list()
+    row.append("CTPPS/TrackingPixel/"+station+plot)
+    rows.append(row)
+
+  CTPPSTrackingPixelLayout(dqmitems, plot, *rows)
+
+for plot in ["4 fired ROCs per BX", "5 fired planes per BX","ROCs hits multiplicity per event","number of fired planes per event","max number of fired aligned_ROCs per event"]:
+  rows = list()
+  for station in pixstations:
+    row = list()
+    row.append("CTPPS/TrackingPixel/"+station+"fr_hr/"+plot)
+    rows.append(row)
+
+  CTPPSTrackingPixelLayout(dqmitems, plot, *rows)
+
+sectors = [ "sector 45", "sector 56" ]
+for plot in ["hits position"]:
+  for sector in sectors:
+    rows = list()
+    row = list()
+    for plane in planes:
+      row.append("CTPPS/TrackingPixel/"+sector+"/station 220/fr_hr/plane_"+plane+"/"+plot)
+    rows.append(row)
+
+    row = list()
+    for plane in planes2:
+      row.append("CTPPS/TrackingPixel/"+sector+"/station 220/fr_hr/plane_"+plane+"/"+plot)
+    rows.append(row)
+
+    CTPPSTrackingPixelLayout(dqmitems, plot+":" +sector+" station 220_fr_hr", *rows)
