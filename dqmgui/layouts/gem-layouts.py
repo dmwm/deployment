936766cb7dce210fe9709f7f94820589df0a7036
Byeonghak Ko <quark2930@hotmail.com>
2017-11-12 19:30:29 +0900

    Modified for new DQM configure

--- dqmgui/layouts/gem-layouts.py
+++ dqmgui/layouts/gem-layouts.py
@@ -1,35 +1,41 @@
 def GEMLayout(i, p, *rows): i["GEM/Layouts/" + p] = DQMItem(layout=rows)
+#def GEMLayout(i, p, *rows): return 1 # this line and the following one line is for printing out indices
+#dqmitems = ""
 
 GEMLayout(dqmitems, "0 eta", [{'path' : "GEM/eta", 'description': 'eta recHit'}])
 GEMLayout(dqmitems, "1 phi", [{'path' : "GEM/phi", 'description': 'phi recHit'}])
 
 layers = ["_la_1", "_la_2"]
-Geminis = ["671093248", "671095040", "671095296", "671095552", "671095808"]
+#Geminis = ["671093248", "671095040", "671095296", "671095552", "671095808"]
+Geminis = ["671088640", "671095296", "671095552", "671095808", "671096064"]
+GeminisId = ["1", "27", "28", "29", "30"]
 index = 2
 
-for gemini in Geminis:
+for i, gemini in enumerate(Geminis):
 	for layer in layers:
 	
-		recHit = "GEM/recHit/recHit_Gemini_" + gemini + layer
-		recHit_index = "'GEM/Layouts/%d recHit per VFAT %s%s," % (index, gemini, layer)
+		recHit = "GEM/recHit/recHit_Gemini_" + GeminisId[ i ] + layer
+		recHit_index = "GEM/Layouts/%d recHit per VFAT %s%s" % (index, gemini, layer)
 		index = index + 1
-		VFAT = "GEM/recHit/VFAT_vs_ClusterSize_Gemini_" + gemini + layer
-		VFAT_index = "'GEM/Layouts/%d VFAT vs ClusterSize %s%s," % (index, gemini, layer)
+		VFAT = "GEM/recHit/VFAT_vs_ClusterSize_Gemini_" + GeminisId[ i ] + layer
+		VFAT_index = "GEM/Layouts/%d VFAT vs ClusterSize %s%s" % (index, gemini, layer)
 		index = index + 1
-		Fired = "GEM/recHit/StripFired_Gemini_" + gemini + layer
-		Fired_index = "'GEM/Layouts/%d FiredStrips %s%s," % (index, gemini, layer)
+		Fired = "GEM/recHit/StripFired_Gemini_" + GeminisId[ i ] + layer
+		Fired_index = "GEM/Layouts/%d FiredStrips %s%s" % (index, gemini, layer)
 		index = index + 1
 		Digi = "GEM/digi/Digi_Strips_Gemini_" + gemini + layer
-		Digi_index = "'GEM/Layouts/%d DigiStrips %s%s," % (index, gemini, layer)
+		Digi_index = "GEM/Layouts/%d DigiStrips %s%s" % (index, gemini, layer)
 		index = index + 1
-# 		print recHit_index
-# 		print VFAT_index
-# 		print Fired_index
-# 		print Digi_index
-		GEMLayout(dqmitems, gemini + layer, 
+		#print "'%s', "%recHit_index
+		#print "'%s', "%VFAT_index
+		#print "'%s', "%Fired_index
+		#print "'%s', "%Digi_index
+		gemid = GeminisId[ i ] if len(GeminisId[ i ]) > 2 else "0" + GeminisId[ i ]
+		#GEMLayout(dqmitems, gemini + layer, 
+		GEMLayout(dqmitems, "GEMINI%sla%s"%(gemid, layer.replace("_la_", "")), 
 			[{'path': recHit, 'description': "Number of recHit per VFAT"},
      		{'path': VFAT, 'description': "VFAT cs ClusterSize"}],
-		    [{'path': Fired, 'description': "Number of Fired Strips"},
+			[{'path': Fired, 'description': "Number of Fired Strips"},
 		    {'path': Digi, 'description': "Number of Digi Strips"}])	
 # 		GEMLayout(dqmitems, recHit_index, [{'path' : recHit, 'description': 'Number of recHit per VFAT'}])
 # 		GEMLayout(dqmitems, VFAT_index, [{'path' : VFAT, 'description': 'VFAT cs ClusterSize'}])
