96691b6b7fc8f1c2ba2a83ec72be95bfa4c55827
fioriNTU <francesco.fiori@cern.ch>
2019-09-25 09:59:36 +0200

    P5cosmicsL1T (#808)
    * adding L1T plots to shifter cosmics layout
    
    * adding L1T plots to the cosmic layout

--- dqmgui/layouts/shift_cosmic_layout.py
+++ dqmgui/layouts/shift_cosmic_layout.py
@@ -9,6 +9,7 @@ quality = "Overview of system quality. Expressed in percentage of chambers."
 occupancy = "Occupancy per sector"
 ###### needed for RPC plots #############
 
+#Summary plots
 
 shiftcosmiclayout(dqmitems, "00 - Tracking ReportSummary",
  [{ 'path': "Tracking/EventInfo/reportSummaryMap",
@@ -29,6 +30,8 @@ shiftcosmiclayout(dqmitems, "03 - CSC ReportSummary",
 shiftcosmiclayout(dqmitems, "04 - RPC ReportSummary",
                [{ 'path': "RPC/EventInfo/reportSummaryMap", 'description': summary + rpclink }])
 
+#tracks
+
 shiftcosmiclayout(dqmitems, "05 - Tracks (Cosmic Tracking)",
  [{ 'path': "Tracking/TrackParameters/GeneralProperties/NumberOfTracks_CKFTk",
     'description': "Number of cosmic tracks  - <a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftSiStrip>DQMShiftOnlineSiStrip</a> ", 'draw': { 'withref': "yes" }},
@@ -42,6 +45,8 @@ shiftcosmiclayout(dqmitems, "05 - Tracks (Cosmic Tracking)",
     'description': "Phi distribution of cosmic tracks -  <a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftSiStrip>DQMShiftOnlineSiStrip</a> ", 'draw': { 'withref': "yes" }},
   { 'path': "Tracking/TrackParameters/GeneralProperties/TrackEta_CKFTk",
     'description': " Eta distribution of cosmic tracks - <a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftSiStrip>DQMShiftOnlineSiStrip</a> ", 'draw': { 'withref': "yes" }}])
+
+#strip quantities
 
 shiftcosmiclayout(dqmitems, "06 - # of Cluster Trend",
   [{ 'path': "SiStrip/MechanicalView/TIB/TotalNumberOfClusterProfile__TIB",
@@ -71,6 +76,8 @@ shiftcosmiclayout(dqmitems, "07 OnTrackCluster",
    { 'path': "SiStrip/MechanicalView/TEC/PLUS/Summary_ClusterStoNCorr_OnTrack__TEC__PLUS",
      'description': "Signal-to-Noise (corrected for the angle) for On-Track clusters in TEC +ve side - <a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftSiStrip>DQMShiftOnlineSiStrip</a> ", 'draw': { 'withref': "yes" } }])
 
+#CSC
+
 shiftcosmiclayout(dqmitems,"08 CSC Physics Efficiency - RecHits Minus",
     [{'path': "CSC/CSCOfflineMonitor/recHits/hRHGlobalm1", 'description': "Histogram shows 2D RecHits distribution in ME-1. Any unusual inhomogeneity should be reported. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/viewauth/CMS/CSCDPGDataMonitorShiftInstructions\">here</a>."},
      {'path': "CSC/CSCOfflineMonitor/recHits/hRHGlobalm2", 'description': "Histogram shows 2D RecHits distribution in ME-2. Any unusual inhomogeneity should be reported. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/viewauth/CMS/CSCDPGDataMonitorShiftInstructions\">here</a>."}],
@@ -83,7 +90,27 @@ shiftcosmiclayout(dqmitems,"09 CSC Physics Efficiency - RecHits Plus",
     [{'path': "CSC/CSCOfflineMonitor/recHits/hRHGlobalp3", 'description': "Histogram shows 2D RecHits distribution in ME+3. Any unusual inhomogeneity should be reported. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/viewauth/CMS/CSCDPGDataMonitorShiftInstructions\">here</a>."},
      {'path': "CSC/CSCOfflineMonitor/recHits/hRHGlobalp4", 'description': "Histogram shows 2D RecHits distribution in ME+4. Any unusual inhomogeneity should be reported. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/viewauth/CMS/CSCDPGDataMonitorShiftInstructions\">here</a>."}])
 
+#RPC
+
 shiftcosmiclayout(dqmitems, "10 - RPC_Occupancy",
                [{ 'path': "RPC/AllHits/SummaryHistograms/Occupancy_for_Barrel", 'description': occupancy + rpclink  }],
                [{ 'path': "RPC/AllHits/SummaryHistograms/Occupancy_for_Endcap", 'description': occupancy + rpclink }])
 
+shiftcosmiclayout(dqmitems, "11 - uGMT MUON P_{T}",
+  [{
+    'path': "L1T/L1TStage2uGMT/ugmtMuonPt",
+    'description': "This should show normal pT spectrum (spikes at 140 GeV, 200 GeV and 255 GeV expected from max TF pT assigned)",
+    'draw': { 'withref': "no" }
+  }])
+shiftcosmiclayout(dqmitems, "12 - uGMT MUON ETA",
+  [{
+    'path': "L1T/L1TStage2uGMT/ugmtMuonEta",
+    'description': "This should have no spikes.",
+    'draw': { 'withref': "no" }
+  }])
+shiftcosmiclayout(dqmitems, "13 - uGMT MUON PHI",
+  [{
+    'path': "L1T/L1TStage2uGMT/ugmtMuonPhi",
+    'description': "This should have no spikes or dips.",
+    'draw': { 'withref': "no" }
+  }])
