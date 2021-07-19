f83a412d972bce4ca58e141e394861d8bdf811f9
Junghwan John Goh <jhgoh@cern.ch>
2021-07-19 17:26:02 +0900

    remove RPC quality state overview plot from the shifter layout which is not well-defined

--- dqmgui/layouts/shift_rpc_layout.py
+++ dqmgui/layouts/shift_rpc_layout.py
@@ -5,7 +5,6 @@ summary = "summary map for rpc, this is NOT an efficiency measurement"
 rpclink = "   >>> <a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftRPC>Description</a>"
 fed = "FED Fatal Errors";
 rpcevents = "Events processed by the RPC DQM"
-quality = "Overview of system quality. Expressed in percentage of chambers."
 occupancy = "Occupancy per sector"
 
 ################### Links to Histograms #################################
@@ -20,9 +19,6 @@ shiftrpclayout(dqmitems, "01-Fatal_FED_Errors",
 shiftrpclayout(dqmitems, "02-RPC_Events",
                [{ 'path': "RPC/AllHits/RPCEvents", 'description': rpcevents + rpclink }])
 
-shiftrpclayout(dqmitems, "03-Quality_State_Overview",
-               [{ 'path': "RPC/AllHits/SummaryHistograms/RPC_System_Quality_Overview", 'description': quality + rpclink }])
-
-shiftrpclayout(dqmitems, "04-RPC_Occupancy",
+shiftrpclayout(dqmitems, "03-RPC_Occupancy",
                [{ 'path': "RPC/AllHits/SummaryHistograms/Occupancy_for_Barrel", 'description': occupancy + rpclink  }],
                [{ 'path': "RPC/AllHits/SummaryHistograms/Occupancy_for_Endcap", 'description': occupancy + rpclink }])
