4de177776fce0f5b7b0cf212231a6d8f0f4c3d80
Junghwan John Goh <jhgoh@cern.ch>
2021-07-23 15:58:48 +0900

    remove RPC quality state overview plot from the shifter layout which is not well-defined (#1064)

--- dqmgui/layouts/shift_rpc_T0_layout.py
+++ dqmgui/layouts/shift_rpc_T0_layout.py
@@ -5,7 +5,6 @@ fed = "FED Fatal Errors. Entries MUST be ZERO at all times. If not, report the p
 rpcevents = "Number of processed events."
 eff = "RPC Efficiency distribution. Make sure average values is greater than 80."
 rpclink = "   >>> <a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftRPC>Description</a>"
-quality = "Overview of system quality. Expressed in percentage of chambers."
 occupancy = "Occupancy per sector"
 hv = "High Voltage status per lumi section. If ALL bins are red mark the run as BAD."
 
@@ -23,19 +22,15 @@ shiftrpclayout(dqmitems, "03-RPC_Events",
                [{ 'path': "RPC/AllHits/RPCEvents", 'description': rpcevents + rpclink }])
 
 
-shiftrpclayout(dqmitems, "04-Quality_State_Overview",
-               [{ 'path': "RPC/AllHits/SummaryHistograms/RPC_System_Quality_Overview", 'description': quality + rpclink }])
-
-
-shiftrpclayout(dqmitems, "05-RPC_Occupancy",
+shiftrpclayout(dqmitems, "04-RPC_Occupancy",
                [{ 'path': "RPC/AllHits/SummaryHistograms/Occupancy_for_Barrel", 'description': occupancy + rpclink  }],
                [{ 'path': "RPC/AllHits/SummaryHistograms/Occupancy_for_Endcap", 'description': occupancy + rpclink  }])
 
 
-shiftrpclayout(dqmitems, "06-Statistics",
+shiftrpclayout(dqmitems, "05-Statistics",
                [{ 'path': "RPC/RPCEfficiency/Statistics", 'description': eff + rpclink }])
 
-shiftrpclayout(dqmitems, "07-Efficiency_Distribution",
+shiftrpclayout(dqmitems, "06-Efficiency_Distribution",
                [{ 'path': "RPC/RPCEfficiency/EffBarrelRoll", 'description': eff + rpclink  }],
                [{ 'path': "RPC/RPCEfficiency/EffEndcapPlusRoll", 'description': eff + rpclink  },
                 { 'path': "RPC/RPCEfficiency/EffEndcapMinusRoll", 'description':  eff + rpclink }])
