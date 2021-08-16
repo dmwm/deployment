7790e48e883703e15b501c1f0446ece7dcd60138
jfernan2 <Javier.Fernandez@cern.ch>
2021-08-16 11:45:42 +0200

    Fix typo

--- dqmgui/style/DTRenderPlugin.cc
+++ dqmgui/style/DTRenderPlugin.cc
@@ -257,8 +257,7 @@ private:
         obj->GetYaxis()->SetNdivisions(6,true);
         obj->GetXaxis()->CenterLabels();
         obj->GetYaxis()->CenterLabels();
-        obj->
-	um(100);
+        obj->SetMaximum(100);
         c->SetGrid(1,1);
         obj->GetXaxis()->SetTitleOffset(1.15);
         c->SetBottomMargin(0.1);
