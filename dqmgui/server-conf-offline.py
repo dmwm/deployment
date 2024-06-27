ac42e68610205b00f527c06f63b92ce101aa5627
Dimitris Papagiannis <d.papag.a@gmail.com>
2024-06-27 09:34:28 +0200

    Update layout access whitelist

--- dqmgui/server-conf-offline.py
+++ dqmgui/server-conf-offline.py
@@ -58,8 +58,9 @@ server.extend(
     STATEDIR,
     [
         "/DC=ch/DC=cern/OU=Organic Units/OU=Users/CN=rovere/CN=653292/CN=Marco Rovere",
-        "/DC=ch/DC=cern/OU=Organic Units/OU=Users/CN=batinkov/CN=739757/CN=Atanas Ivanov Batinkov",
-        "/DC=ch/DC=cern/OU=Organic Units/OU=Users/CN=bvanbesi/CN=759373/CN=Broen van Besien",
+        "/DC=ch/DC=cern/OU=Organic Units/OU=Users/CN=dpapagia/CN=857294/CN=Dimitrios Papagiannis",
+        "/DC=ch/DC=cern/OU=Organic Units/OU=Users/CN=dpapagia/CN=857294/CN=Dimitris Papagiannis",
+        "/DC=ch/DC=cern/OU=Organic Units/OU=Users/CN=gamoreir/CN=844403/CN=Gabriel Moreira Da Silva Campos",
     ],
 )
 server.source("DQMUnknown")
