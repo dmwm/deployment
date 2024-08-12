6d5a66d0f90a877f4eaae64ec24866817b164728
Dimitris Papagiannis <d.papag.a@gmail.com>
2024-08-12 18:33:43 +0200

    Cleanup kinit call

--- dqmgui/kinit.sh
+++ dqmgui/kinit.sh
@@ -6,8 +6,7 @@ export keytab=""
 # Skip the .service principals
 principal=$(klist -k "$keytab" | grep -v '.service' | tail -1 | grep -oE '[[:alnum:]_-]+@CERN\.CH')
 kdestroy -A >/dev/null 2>&1
-kinit $principal -k -t "$keytab" 2>&1 1>&/dev/null
-if [ $? = 1 ]; then
+if ! kinit $principal -k -t "$keytab" 2>&1 1>&/dev/null; then
     echo "Unable to perform kinit for ${principal}."
     echo "If you are installing a DQM GUI on lxplus or any other private machine, please comment lines from this block and proceed as usual"
     exit 1
