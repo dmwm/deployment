c2a2b1783c4c86db13e665417c6be32cd7840448
Dimitris Papagiannis <d.papag.a@gmail.com>
2024-08-13 12:21:53 +0200

    Add tries to kinit script

--- dqmgui/kinit.sh
+++ dqmgui/kinit.sh
@@ -6,7 +6,19 @@ export keytab=""
 # Skip the .service principals
 principal=$(klist -k "$keytab" | grep -v '.service' | tail -1 | grep -oE '[[:alnum:]_-]+@CERN\.CH')
 kdestroy -A >/dev/null 2>&1
-if ! kinit $principal -k -t "$keytab" 2>&1 1>&/dev/null; then
+
+# Try up to # tries times to run kinit.
+tries=${1:-1}
+while [ $tries -gt 0 ]; do
+    if ! kinit $principal -k -t "$keytab" 2>&1 1>&/dev/null; then
+        tries=$((tries - 1))
+        sleep 4
+    else
+        break
+    fi
+done
+
+if [ $tries -eq 0 ]; then
     echo "Unable to perform kinit for ${principal}."
     echo "If you are installing a DQM GUI on lxplus or any other private machine, please comment lines from this block and proceed as usual"
     exit 1
